39dbcf032c0a0a1e7cdffe0db9fe880fc89b8b11
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-14 14:24:18 +0100

    Create 100.Spatial_Data.R

---
+++ 100.Spatial_Data.R
@@ -0,0 +1,31 @@
+install.packages(c("sf", "sp", "raster", "ncdf4", "exactextractr", "dplyr", "reshape2", "ggplot2", "ggrepel", "zoo"))
+
+setwd("D:Personale/Studio/^Magistrale_Tesi/zDatasets/???????")
+
+suppressPackageStartupMessages({
+library(sf)              # useful for spatial manipulations
+library(sp)              # useful for spatial manipulations
+library(raster)          # useful for working with raster data
+library(ncdf4)           # useful for working with raster data
+library(exactextractr)   # useful for extracting data from raster files
+library(dplyr)           # useful for merging data sets
+library(reshape2)        # useful for manipulating data sets
+library(ggplot2)         # useful for data visualization
+library(ggrepel)         # useful for labeling point plots in ggplot2
+library(zoo)   
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
