875371be861c6049c7eb12dfbbb121e7943ddbb5
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-19 16:30:40 +0100

    Update 100.ISIMIP_Roman.R

--- 100.ISIMIP_Roman.R
+++ 100.ISIMIP_Roman.R
@@ -23,8 +23,8 @@ names(r) <- paste0(rep(paste0(1901:2018), each=12), "-", stringr::str_pad(rep(1:
 plot.df <- as.data.frame(r[[12]], xy = TRUE)  ## 12 equivale a dicembre 1901
 plot.df <- plot.df[complete.cases(plot.df), ]
 
-
 gw_data <- exactextractr::exact_extract(r, shp, fun="mean")
+gw_data$region = shp$ADMIN_NAME
 shp$gw_1902_12 <- gw_data$mean.X1901.12.2
 
 ggplot(shp, aes(fill=gw_1902_12)) + 
@@ -35,6 +35,35 @@ ggplot(shp, aes(fill=gw_1902_12)) +
 
 
 
+
+# PLOT DIFFERENT REGIONS
+plot.df <- reshape2::melt(gw_data, id.vars="region")
+plot.df$date <- zoo::as.Date(zoo::as.yearmon(substr(as.character(plot.df$variable), 7, 13), "%Y.%m"))
+ggplot(subset(plot.df, region %in% c("Red Sea", "Kassala", "Northern", "Al Gezira")), 
+       # date on x axis, spei (stored in variable "value") on y axis, color (of outlines) based on spei, filling based on spei
+       aes(date, value, fill=value, col=value)) +   
+  # add a horizontal line at zero
+  geom_hline(yintercept=0) +                  
+  # add a horizontal, dotted (lty=3) line at -1.5 to visualize the threshold for severe drought
+  geom_hline(yintercept=-1.5, lty=3) +     
+  # split the plot into separate plots for each region, organized in two columns
+  facet_wrap(region~., ncol=2) +        
+  # nicer theme than standard theme
+  theme_bw() +         
+  # white background for province names
+  theme(strip.background=element_rect(fill="white")) +          
+  # y axis title
+  ylab("GW Storage") +                                                                
+  # x axis title
+  xlab("") +                                                                       
+  # columns for GW Storage in each month
+  geom_col() +                                                                      
+  # set color palette (for the outlines of the columns)
+  scale_fill_viridis_c(option="inferno", end  = 0.8) +                              
+  # set fill palette (for filling the columns)
+  scale_color_viridis_c(option="inferno", end = 0.8) +                              
+  # set legend title
+  labs(fill="SPEI-12",col="GW Storage") 
 
 
 
