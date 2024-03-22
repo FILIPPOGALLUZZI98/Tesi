34cfdbeb06256cf9d2552546fb8606538407eb50
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-22 11:57:50 +0100

    Create 100.Annual_Mean.R

---
+++ 100.Annual_Mean.R
@@ -0,0 +1,23 @@
+# Medie annuali dei dati raster ISIMIP3a del groundwstr a partire dai dati mensili
+r <- raster::brick("GW_Data/ISIMIP3a/cwatm_gswp3-w5e5_obsclim_histsoc_default_groundwstor_global_monthly_1901_2019.nc")
+
+# Creazione di un nuovo raster brick per le medie annuali
+gwy <- raster::brick(ncol = ncol(r), nrow = nrow(r), nl = nlayers(r)/12,
+xmn = extent(r)[1], xmx = extent(r)[2], 
+ymn = extent(r)[3], ymx = extent(r)[4], 
+crs = crs(r))
+
+# Ciclo for per calcolare le medie annuali
+for (i in 1:(nlayers(r)/12)) {
+  start <- (i - 1) * 12 + 1
+  end <- start + 11
+  yearly_mean <- calc(r[[start:end]], mean)
+  gwy[[i]] <- yearly_mean
+}
+
+# Salvataggio dati
+years <- unique(format(as.Date(names(r), format = "X%Y.%m.%d"), "%Y"))
+names(gwy) <- paste0("gw", years)
+output_nc <- "GW_Data/ISIMIP3a/gwy.nc"
+# Scrivi il RasterBrick in un file NetCDF
+writeRaster(gwy, filename = output_nc, format = "CDF", overwrite = TRUE)
