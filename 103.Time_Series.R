2c4fc9dfbcc286cf3e4c48728100e1e5d44c3239
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-22 16:04:11 +0100

    Update 103.Time_Series.R

--- 103.Time_Series.R
+++ 103.Time_Series.R
@@ -1,15 +1,16 @@
-stato <- "Italy"
+stato <- "Israel"
 
 state <- subset(shp, CNTRY_NAME == stato)
 gw_data <- exactextractr::exact_extract(r, state, fun="mean")
 
 gw_data$region <- state$ADMIN_NAME
-plot.df <- reshape2::melt(gw_data, id.vars="region")
+time_ser <- reshape2::melt(gw_data, id.vars="region")
+time_ser$date <- 1901 + seq(0, 118)
 
 R <- c(gw_data$region)
-ggplot(subset(plot.df, region %in% R), 
+ggplot(subset(time_ser, region %in% R), 
        # date on x axis, spei (stored in variable "value") on y axis, color (of outlines) based on spei, filling based on spei
-       aes(variable, value, fill=value, col=value)) +   
+       aes(date, value, fill=value, col=value)) +   
   # add a horizontal line at zero
   geom_hline(yintercept=0) +                  
   # add a horizontal, dotted (lty=3) line at -1.5 to visualize the threshold for severe drought
@@ -29,9 +30,7 @@ ggplot(subset(plot.df, region %in% R),
   # set color palette (for the outlines of the columns)
   scale_fill_viridis_c(option="inferno", end  = 0.8) +                              
   # set fill palette (for filling the columns)
-  scale_color_viridis_c(option="inferno", end = 0.8) +                              
-  # set legend title
-  labs(fill="SPEI-12",col="SPEI-12")  
+  scale_color_viridis_c(option="inferno", end = 0.8)
 
 
 
