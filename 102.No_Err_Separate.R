a19782fe48223c041697bb64a3f2942b9ab5d779
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-22 12:32:05 +0100

    Update 102.No_Err_Separate.R

--- 102.No_Err_Separate.R
+++ 102.No_Err_Separate.R
@@ -54,7 +54,7 @@ ggplot(state[[1]], aes(fill=gw_data[[1]]$mean.X117)) +
 
 
 ################################################################################################
-####  RIMOZIONE CONFLITTI E PLOT  ##############################################################
+####  RIMOZIONE STATI CON ERRORE  ##############################################################
 gw_data <- list()
 state <- list()
 nomi <- unique(shp$CNTRY_NAME)
@@ -90,7 +90,25 @@ ggplot(state[[x]], aes(fill=gw_data[[x]]$mean.X117)) +
 
 
 #################################################################################################
-####  PLOT PER TUTTI GLI STATI SEPARATI  ########################################################
+#################################################################################################
+#################################################################################################
+####  RIMOZIONE SOLO REGIONI CON ERRORE  ########################################################
+
+# Upload dei dati shapefile + settaggio coordinate
+shp <- sf::read_sf("GW_Data/world_geolev1_2021/world_geolev1_2021.shp")
+shp <- sf::st_transform(shp, sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")) 
+
+# Upload dati delle medie annuali (non diretti ISIMIP3a) + settaggio coordinate
+r <- raster::brick("GW_Data/ISIMIP3a/gwy.nc")
+proj4string(r) <- raster::crs(shp)
+
+# Voglio rimuovere soltanto le singole regioni degli stati che hanno geometry EMPTY, in modo da
+# poter usare le regioni rimanenti
+# Cerco il numero della posizione delle regioni dei paesi che hanno geometry EMPTY
+elementi <- c(34, 381, 2070, 419, 420, 2071, 643, 770, 868, 930, 2072, 1065, 
+              1105, 1162, 1542, 1548, 1578, 1824, 1968, 2073)
+# Rimuovo questi elementi da shp
+shp<- shp[-elementi,]
 
 gw_data <- list()
 state <- list()
