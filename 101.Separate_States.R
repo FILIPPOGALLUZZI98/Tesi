d5ea0ad4bc7ee628198bf80e2d3b205dd774b1d5
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-22 12:52:29 +0100

    Create 101.Separate_States.R

---
+++ 101.Separate_States.R
@@ -0,0 +1,36 @@
+# Per vedere meglio il procedimento vedi '002.Prove_Varie.R'
+
+shp <- sf::read_sf("GW_Data/world_geolev1_2021/world_geolev1_2021.shp")
+shp <- sf::st_transform(shp, sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")) 
+elementi <- c(34, 381, 2070, 419, 420, 2071, 643, 770, 868, 930, 2072, 1065, 
+              1105, 1162, 1542, 1548, 1578, 1824, 1968, 2073)
+shp<- shp[-elementi,]
+r <- raster::brick("GW_Data/ISIMIP3a/gwy.nc")
+proj4string(r) <- raster::crs(shp)
+
+
+gw_data <- list()
+state <- list()
+nomi <- unique(shp$CNTRY_NAME)
+
+for (i in seq_along(nomi)) {
+  a <- subset(shp, CNTRY_NAME == nomi[i])
+  state <- append(state, list(a))
+}
+
+for (i in 1:264) {
+  b <- exactextractr::exact_extract(r, state[[i]], fun="mean")
+  b$region <- state[[i]]$ADMIN_NAME
+  gw_data <- append(gw_data, list(b))
+}
+
+
+# Selezionare il nome del paese che voglio plottare
+country <- "Nigeria"
+x <- which(nomi == country)
+# Selezionare l'anno ricordando che mean.X1<-->1901 e così via
+ggplot(state[[x]], aes(fill=gw_data[[x]]$mean.X1)) + 
+  geom_sf(col="black") +
+  theme_bw() +
+  labs(fill="gw storage") +
+  scale_fill_viridis_c(option="viridis", end=0.8)
