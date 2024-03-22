37304dfe9b4e285beaed6c485be228c448be429a
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-22 15:55:18 +0100

    Update 103.Time_Series.R

--- 103.Time_Series.R
+++ 103.Time_Series.R
@@ -1,15 +1,14 @@
-#################################################################################################
-####  PLOT TIME SERIES  ##########################################################################
-plot.df <- as.data.frame(r[[119]], xy = TRUE)  ## 119 equivale a dicembre 2019
-plot.df <- plot.df[complete.cases(plot.df), ]
+stato <- "Italy"
+state <- subset(shp, CNTRY_NAME == stato)
+gw_data <- exactextractr::exact_extract(r, state, fun="mean")
+
+gw_data$region <- state$ADMIN_NAME
 plot.df <- reshape2::melt(gw_data, id.vars="region")
-plot.df$date <- zoo::as.Date(zoo::as.yearmon(substr(as.character(plot.df$variable), 7, 13), "%Y.%m"))
 
-
-# Plottiamo le serie temporali per 4 regioni del Sudan
-ggplot(subset(plot.df, region %in% c("Red Sea", "Kassala", "Northern", "Al Gezira")), 
+R <- c(gw_data$region)
+ggplot(subset(plot.df, region %in% R), 
        # date on x axis, spei (stored in variable "value") on y axis, color (of outlines) based on spei, filling based on spei
-       aes(date, value, fill=value, col=value)) +   
+       aes(variable, value, fill=value, col=value)) +   
   # add a horizontal line at zero
   geom_hline(yintercept=0) +                  
   # add a horizontal, dotted (lty=3) line at -1.5 to visualize the threshold for severe drought
@@ -21,15 +20,19 @@ ggplot(subset(plot.df, region %in% c("Red Sea", "Kassala", "Northern", "Al Gezir
   # white background for province names
   theme(strip.background=element_rect(fill="white")) +          
   # y axis title
-  ylab("GW Storage") +                                                                
+  ylab("SPEI-12") +                                                                
   # x axis title
   xlab("") +                                                                       
-  # columns for GW Storage in each month
+  # columns for SPEI in each month
   geom_col() +                                                                      
   # set color palette (for the outlines of the columns)
   scale_fill_viridis_c(option="inferno", end  = 0.8) +                              
   # set fill palette (for filling the columns)
-  scale_color_viridis_c(option="inferno", end = 0.8)
+  scale_color_viridis_c(option="inferno", end = 0.8) +                              
+  # set legend title
+  labs(fill="SPEI-12",col="SPEI-12")  
+
+
 
 
 
