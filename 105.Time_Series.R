7f395b6af0c29b049b47665a2554df60208b4829
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-22 12:11:21 +0100

    Update and rename 104.Time_Series.R to 105.Time_Series.R

--- 105.Time_Series.R
+++ 105.Time_Series.R
@@ -1,18 +1,3 @@
-suppressPackageStartupMessages({
-  library(sf)              ## useful for spatial manipulations
-  library(sp)              ## useful for spatial manipulations
-  library(raster)          ## useful for working with raster data
-  library(ncdf4)           ## useful for working with raster data
-  library(exactextractr)   ## useful for extracting data from raster files
-  library(dplyr)           ## useful for merging data sets
-  library(reshape2)        ## useful for manipulating data sets
-  library(ggplot2)         ## useful for data visualization
-  library(ggrepel)         ## useful for labeling point plots in ggplot2
-  library(lubridate)
-  library(zoo)   
-})
-
-
 #################################################################################################
 ####  PLOT TIME SERIES  ##########################################################################
 plot.df <- as.data.frame(r[[119]], xy = TRUE)  ## 119 equivale a dicembre 2019
