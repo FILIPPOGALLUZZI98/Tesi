6330acb5ed5fde98bbd612cdde4df4cb76e22405
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-25 13:23:23 +0100

    Update 201.Conflict_Libia.R

--- 201.Conflict_Libia.R
+++ 201.Conflict_Libia.R
@@ -1,35 +1,39 @@
-libia <- read.csv("GW_Data/Conflict_Data/Libia.csv")
-state <- subset(shp, CNTRY_NAME == "Libya")
-libia <- libia[, c("relid", "code_status","type_of_violence","latitude" ,"longitude")]
+country <- "Nigeria"
+
+file_path <- paste("GW_Data/Conflict_Data/", country, ".csv", sep = "")
+cntry <- read.csv(file_path)
+state <- subset(shp, CNTRY_NAME == country)
+
+cntry <- cntry[, c("relid", "code_status","type_of_violence","latitude" ,"longitude")]
 gw_data <- exactextractr::exact_extract(r, state, fun="mean")
 gw_data$region <- state$ADMIN_NAME
 
 
 # Per estrarre i valori della longitudine 
-lib <- libia %>%
+cty <- cntry %>%
   mutate(
     longitude = as.numeric(str_extract(stringr::str_extract(longitude, " \\d+\\.\\d+"), "\\d+\\.\\d+"))
   )
 # Elimino le righe con valori NA di latitudine e longitudine
-lib <- na.omit(lib[, c("relid", "code_status","type_of_violence","latitude" ,"longitude")])
+cty <- na.omit(cty[, c("relid", "code_status","type_of_violence","latitude" ,"longitude")])
 
 
 # Imposto lo stesso CRS dello shapefile sui punti
-lib <- st_as_sf(lib, coords = c("longitude", "latitude"), crs = st_crs(state))
-st_set_crs(lib, st_crs(state))
+cty <- st_as_sf(cty, coords = c("longitude", "latitude"), crs = st_crs(state))
+st_set_crs(cty, st_crs(state))
 
-lib <- lib %>%
+cty <- cty %>%
   mutate(
     latitude = as.numeric(str_extract(geometry, "\\d+\\.\\d+")),
     longitude = as.numeric(str_extract(stringr::str_extract(geometry, " \\d+\\.\\d+"), "\\d+\\.\\d+"))
   )
-lib <- subset(lib, select = -geometry)
+cty <- subset(cty, select = -geometry)
 
 ggplot(state) +           
   # plot the outlines of the shapefile of italy (using black borders and transparent filling)
   geom_sf(fill = NA, col = "black") +  
   # plot points for each event, using longitude, latitude as x and y, set shape and color of points using variable "event type" (which is either protests or riots)
-  geom_point(data = lib, aes(longitude, latitude, color = factor(code_status)), size = 1) +
+  geom_point(data = cty, aes(longitude, latitude, color = factor(code_status)), size = 1) +
   # Assegna manualmente i colori ai valori di code_status
   scale_color_manual(values = c("1" = "red", "2" = "blue", "3" = "green"))
   theme_bw() +                     
