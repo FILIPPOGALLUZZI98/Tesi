90006b7506f6b7b083e6b02df7bd9004abdb59b6
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-25 13:43:03 +0100

    Update 201.Conflict_Single_Country.R

--- 201.Conflict_Single_Country.R
+++ 201.Conflict_Single_Country.R
@@ -16,44 +16,44 @@ proj4string(r) <- raster::crs(shp)
 country <- "Nigeria"
 
 file_path <- paste("GW_Data/Conflict_Data/", country, ".csv", sep = "")
-cntry <- read.csv(file_path)
+events <- read.csv(file_path)
 state <- subset(shp, CNTRY_NAME == country)
 gw_data <- exactextractr::exact_extract(r, state, fun="mean")
 gw_data$region <- state$ADMIN_NAME
 
 # Seleziono soltanto le variabili che mi interessano
-cntry <- cntry[, c("relid", "code_status","type_of_violence","latitude" ,"longitude")]
+events <- events[, c("relid", "code_status","type_of_violence","latitude" ,"longitude")]
 
 
 
 
 # Per impostare lo stesso crs devo prima separare i valori della longitudine dalla latitudine
-cntry <- cntry %>%
+events <- events %>%
   mutate(
     longitude = as.numeric(str_extract(stringr::str_extract(longitude, " \\d+\\.\\d+"), "\\d+\\.\\d+"))
   )
 # Elimino le righe con valori NA di latitudine e longitudine
-cntry <- na.omit(cntry[, c("relid", "code_status","type_of_violence","latitude" ,"longitude")])
+events <- na.omit(events[, c("relid", "code_status","type_of_violence","latitude" ,"longitude")])
 # Imposto lo stesso CRS dello shapefile sui punti
-cntry <- st_as_sf(cntry, coords = c("longitude", "latitude"), crs = st_crs(state))
-st_set_crs(cntry, st_crs(state))
-cntry <- cntry %>%
+events <- st_as_sf(events, coords = c("longitude", "latitude"), crs = st_crs(state))
+st_set_crs(events, st_crs(state))
+events <- events %>%
   mutate(
     latitude = as.numeric(str_extract(geometry, "\\d+\\.\\d+")),
     longitude = as.numeric(str_extract(stringr::str_extract(geometry, " \\d+\\.\\d+"), "\\d+\\.\\d+"))
   )
-cntry <- subset(cntry, select = -geometry)
+events <- subset(events, select = -geometry)
 
 
 
 
 
-# cntry è il dataset dei punti del paese che ho selezionato nel giusto crs
+# events è il dataset dei punti del paese che ho selezionato nel giusto crs
 ggplot(state) +           
   # plot the outlines of the shapefile of italy (using black borders and transparent filling)
   geom_sf(fill = NA, col = "black") +  
   # plot points for each event, using longitude, latitude as x and y, set shape and color of points using variable "event type" (which is either protests or riots)
-  geom_point(data = cntry, aes(longitude, latitude, color = factor(code_status)), size = .5) +
+  geom_point(data = events, aes(longitude, latitude, color = factor(code_status)), size = .5) +
   # Assegna manualmente i colori ai valori di code_status
   scale_color_manual(values = c("1" = "red", "2" = "blue", "3" = "green"))
   theme_bw() +                     
