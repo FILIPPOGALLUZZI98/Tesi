72e0a34d2753d601eef0d42022ae46ef7be37e11
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-25 13:11:29 +0100

    Update 100.Pacchetti.R

--- 100.Pacchetti.R
+++ 100.Pacchetti.R
@@ -6,6 +6,7 @@ suppressPackageStartupMessages({
   library(ncdf4)           ## useful for working with raster data
   library(exactextractr)   ## useful for extracting data from raster files
   library(dplyr)           ## useful for merging data sets
+  library(stringr)
   library(reshape2)        ## useful for manipulating data sets
   library(ggplot2)         ## useful for data visualization
   library(ggrepel)         ## useful for labeling point plots in ggplot2
