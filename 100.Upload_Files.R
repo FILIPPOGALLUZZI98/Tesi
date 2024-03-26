8686e02eb47c115b72f137876ca8117bc5983a08
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-26 09:35:39 +0100

    Create 100.Upload_Files.R

---
+++ 100.Upload_Files.R
@@ -0,0 +1,11 @@
+shp <- sf::read_sf("GW_Data/world_geolev1_2021/world_geolev1_2021.shp")
+shp <- sf::st_transform(shp, sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")) 
+# Rimuovo le regioni che danno geometry error
+elementi <- c(34, 381, 2070, 419, 420, 2071, 643, 770, 868, 930, 2072, 1065, 
+              1105, 1162, 1542, 1548, 1578, 1824, 1968, 2073)
+shp<- shp[-elementi,]
+# Raster è quello modificato da me medie annuali e non mensili
+r <- raster::brick("GW_Data/ISIMIP3a/gwy.nc")  ## groundwstrg
+## r <- raster::brick("GW_Data/ISIMIP3a/twsy.nc")  ## total water storage
+## r <- raster::brick("GW_Data/ISIMIP3a/qry.nc")  ## runoff (??)
+proj4string(r) <- raster::crs(shp)
