559b7fb4bcf86c33d325dca6141533d44c0c0304
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-26 13:38:34 +0100

    Update 002.Data_Upload.R

--- 002.Data_Upload.R
+++ 002.Data_Upload.R
@@ -1,1 +1,6 @@
-
+rs <- raster::brick("GW_Data/ISIMIP3a/gwy.nc")  ## groundwstrg
+rt <- raster::brick("GW_Data/ISIMIP3a/twsy.nc")  ## total water storage
+rq <- raster::brick("GW_Data/ISIMIP3a/qry.nc")  ## runoff (??)
+proj4string(rs) <- raster::crs(shp)
+proj4string(rt) <- raster::crs(shp)
+proj4string(rq) <- raster::crs(shp)
