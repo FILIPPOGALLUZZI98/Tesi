93df739c50332d27677c2fbd310db0981bb155ae
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-26 13:53:32 +0100

    Update 003.Controllo_Geometrie.R

--- Preparazione/003.Controllo_Geometrie.R
+++ Preparazione/003.Controllo_Geometrie.R
@@ -1,22 +1,15 @@
 shp <- sf::read_sf("GW_Data/world_geolev1_2021/world_geolev1_2021.shp")
-shp <- sf::st_transform(shp, sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")) 
-# Rimuovo le regioni che danno geometry error
-elementi <- c(34, 381, 2070, 419, 420, 2071, 643, 770, 868, 930, 2072, 1065, 
-              1105, 1162, 1542, 1548, 1578, 1824, 1968, 2073)
-shp<- shp[-elementi,]
-# Raster è quello modificato da me medie annuali e non mensili
-r <- raster::brick("GW_Data/ISIMIP3a/gwy.nc")  ## groundwstrg
-## r <- raster::brick("GW_Data/ISIMIP3a/twsy.nc")  ## total water storage
-## r <- raster::brick("GW_Data/ISIMIP3a/qry.nc")  ## runoff (??)
-proj4string(r) <- raster::crs(shp)
+shp <- sf::st_transform(shp, sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
 
+# Groundwater Storage
+rs <- raster::brick("Data/GW.gwy.nc"); proj4string(rs) <- raster::crs(shp)
+# Total Water Storage
+rt <- raster::brick("Data/GW.twsy.nc"); proj4string(rt) <- raster::crs(shp)
+# Runoff (??)
+rq <- raster::brick("Data/GW.qry.nc"); proj4string(rq) <- raster::crs(shp)
 
-
-plot(shp[,"geometry"]) 
-ext <- extent(-130, -60, 20, 50)  ## xmin, xmax, ymin, ymax
-rc <- crop(r, ext)
-state <- subset(shp, CNTRY_NAME == "United States")
-plot(rc$X1)
+plot(rs$X1)
+plot(shp[,"geometry"], add=TRUE) 
 plot(state, add = TRUE)
 
 
