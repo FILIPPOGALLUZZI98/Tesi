18d9599218da13ef32f7db59f4cc49dfb71f9aaa
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-26 13:54:56 +0100

    Update 100.Upload_Data.R

--- 100.Upload_Data.R
+++ 100.Upload_Data.R
@@ -3,11 +3,11 @@ shp <- sf::read_sf("GW_Data/world_geolev1_2021/world_geolev1_2021.shp")
 shp <- sf::st_transform(shp, sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
 
 # Groundwater Storage
-rs <- raster::brick("Data/GW.gwy.nc"); proj4string(rs) <- raster::crs(shp)
+rs <- raster::brick("Data/GW/gwy.nc"); proj4string(rs) <- raster::crs(shp)
 # Total Water Storage
-rt <- raster::brick("Data/GW.twsy.nc"); proj4string(rt) <- raster::crs(shp)
+rt <- raster::brick("Data/GW/twsy.nc"); proj4string(rt) <- raster::crs(shp)
 # Runoff (??)
-rq <- raster::brick("Data/GW.qry.nc"); proj4string(rq) <- raster::crs(shp)
+rq <- raster::brick("Data/GW/qry.nc"); proj4string(rq) <- raster::crs(shp)
 
 
 
