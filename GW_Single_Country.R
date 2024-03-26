ced7fd4b1dd912fd65e04a6f3a865988d17a7131
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-26 14:11:12 +0100

    Update GW_Single_Country.R

--- GW_Single_Country.R
+++ GW_Single_Country.R
@@ -1,18 +1,26 @@
+# Scegliere quale raster usare (rs, rt, rq)
+r <- rs
+
+# Selezionare il paese e anno
 country <- "Nigeria"
+y <- 1990
+
 
 # Subset dello shapefile per il paese selezionato
 state <- subset(shp, CNTRY_NAME == country)
 # Media dei valori del raster sulle regioni 
-gw_data_state <- exactextractr::exact_extract(rs, state, fun="mean")
+gw_data_sc <- exactextractr::exact_extract(r, state, fun="mean")
 # Aggiungo una colonna region al file gw_data_state
-gw_data_state$region <- state$ADMIN_NAME
+gw_data_sc$region <- state$ADMIN_NAME
+
+
 
 
 
 # Rinomino gli anni
 for (year in 1901:2019) {
   col_name <- paste0("gw_", year)
-  state[[col_name]] <- gw_data[[paste0("mean.X", year - 1900)]]
+  state[[col_name]] <- gw_data_sc[[paste0("mean.X", year - 1900)]]
 }
 
 # Plot anno 2000
