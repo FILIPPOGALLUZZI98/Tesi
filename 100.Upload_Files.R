921b30ae91690257b4559764bcf2cbb9443a2adb
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-26 09:38:12 +0100

    Update 100.Upload_Files.R

--- 100.Upload_Files.R
+++ 100.Upload_Files.R
@@ -5,7 +5,10 @@ elementi <- c(34, 381, 2070, 419, 420, 2071, 643, 770, 868, 930, 2072, 1065,
               1105, 1162, 1542, 1548, 1578, 1824, 1968, 2073)
 shp<- shp[-elementi,]
 # Raster è quello modificato da me medie annuali e non mensili
-r <- raster::brick("GW_Data/ISIMIP3a/gwy.nc")  ## groundwstrg
-## r <- raster::brick("GW_Data/ISIMIP3a/twsy.nc")  ## total water storage
-## r <- raster::brick("GW_Data/ISIMIP3a/qry.nc")  ## runoff (??)
+rg <- raster::brick("GW_Data/ISIMIP3a/gwy.nc")  ## groundwstrg
+rt <- raster::brick("GW_Data/ISIMIP3a/twsy.nc")  ## total water storage
+rq <- raster::brick("GW_Data/ISIMIP3a/qry.nc")  ## runoff (??)
 proj4string(r) <- raster::crs(shp)
+
+r <- rg  ## Nei codici seguenti il file raster sarà chiamato 'r', quindi 
+         ## devo scegliere quale usare
