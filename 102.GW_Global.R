4a84760813e936eedb0ef01d1ba0c040b220f5de
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-26 15:00:37 +0100

    Update and rename All_States_Joint.R to 102.GW_Global.R

---
+++ 102.GW_Global.R
@@ -0,0 +1,37 @@
+# Scegliere quale raster usare (rs, rt, rq)
+r <- rs
+# Selezionare l'anno
+y <- 1901
+
+
+# Unisco direttamente i valori di GW direttamente sulle regioni
+gw_data_g <- exactextractr::exact_extract(r, shp, fun="mean")
+gw_data_g$region <- shp$ADMIN_NAME
+gw_data_g <- reshape2::melt(gw_data_g, id.vars="region")
+
+
+ggplot(shp, aes(fill=gw_data[[paste0("mean.X", y - 1900)]])) + 
+  geom_sf(col="black") +
+  theme_bw() +
+  labs(fill=paste("groundwstrg", y)) +
+  scale_fill_viridis_c(option="viridis", end=0.8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
