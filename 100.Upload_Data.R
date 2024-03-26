e7010e98f1b5ce5f9868b16fdee3d69c6244a090
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-26 14:47:25 +0100

    Update 100.Upload_Data.R

--- 100.Upload_Data.R
+++ 100.Upload_Data.R
@@ -9,5 +9,19 @@ rt <- raster::brick("Data/GW/twsy.nc"); proj4string(rt) <- raster::crs(shp)
 # Runoff (??)
 rq <- raster::brick("Data/GW/qry.nc"); proj4string(rq) <- raster::crs(shp)
 
+########################################################################################################
+########################################################################################################
+
+##  shp -----------> file contenente le geometrie delle regioni
+##  rs  -----------> file contenente i rasterbrick dell GW storage
+##  rt  -----------> file contenente i rasterbrick del TWS
+##  rq  -----------> file contenente i rasterbrick del runoff
+##  
+##  
+##  
+##  
+##  
+##  
+##  
 
 
