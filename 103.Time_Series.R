b5484ae178d8dcba79ddd8b891857929e52a6ce6
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-26 10:17:43 +0100

    Update 103.Time_Series.R

--- 103.Time_Series.R
+++ 103.Time_Series.R
@@ -11,6 +11,7 @@ time_ser <- reshape2::melt(gw_data, id.vars="region")
 time_ser$date <- 1901 + seq(0, 118)
 
 R <- c(gw_data$region)
+# R <- c("", "", "", "", "")
 ggplot(subset(time_ser, region %in% R), 
        # date on x axis, spei (stored in variable "value") on y axis, color (of outlines) based on spei, filling based on spei
        aes(date, value, fill=value, col=value)) +   
