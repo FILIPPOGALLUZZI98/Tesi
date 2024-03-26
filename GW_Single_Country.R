3de4f1643e75299a622b275594be851218632a0a
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-26 14:38:30 +0100

    Update GW_Single_Country.R

--- GW_Single_Country.R
+++ GW_Single_Country.R
@@ -1,9 +1,11 @@
 # Scegliere quale raster usare (rs, rt, rq)
 r <- rs
-
 # Selezionare il paese e anno
 country <- "Nigeria"
 y <- 1990
+
+
+
 
 
 # Subset dello shapefile per il paese selezionato
@@ -13,18 +15,19 @@ gw_data_sc <- exactextractr::exact_extract(r, state, fun="mean")
 # Aggiungo una colonna region al file gw_data_state
 gw_data_sc$region <- state$ADMIN_NAME
 
+gw_data_sc <- reshape2::melt(gw_data_sc, id.vars="region")
+anni <- 1901:2019
+gw_data_sc <- gw_data_sc %>%
+  group_by(region) %>%
+  mutate(year = anni)
+gw_data_sc$variable=NULL
 
 
+state <- left_join(state, gw_data_sc, by=c("ADMIN_NAME"="region")) 
 
-
-# Rinomino gli anni
-for (year in 1901:2019) {
-  col_name <- paste0("gw_", year)
-  state[[col_name]] <- gw_data_sc[[paste0("mean.X", year - 1900)]]
-}
-
-# Plot anno 2000
-ggplot(state, aes(fill=gw_2000)) + 
+# Plot anno scelto
+data <- subset(state, year == y)
+ggplot(data, aes(fill=value)) + 
   geom_sf(col="black") +
   theme_bw() +
   labs(fill="gw storage") +
