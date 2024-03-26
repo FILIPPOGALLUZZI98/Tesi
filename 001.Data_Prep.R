5c2cf94187bf34fcd98eee093cc80a89a3b4f2e9
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-26 13:48:39 +0100

    Update 001.Data_Prep.R

--- 001.Data_Prep.R
+++ 001.Data_Prep.R
@@ -1,4 +1,18 @@
 #################################################################################################
+####  SHAPEFILE  ############################################################################
+shp <- sf::read_sf("Data_Raw/world_geolev1_2021/world_geolev1_2021.shp")
+# Rimuovo le regioni che danno geometry error
+elementi <- c(34, 381, 2070, 419, 420, 2071, 643, 770, 868, 930, 2072, 1065, 
+              1105, 1162, 1542, 1548, 1578, 1824, 1968, 2073)
+shp<- shp[-elementi,]
+# Rimuovo le variabili che non mi servono
+shp$BPL_CODE <- NULL; shp$CNTRY_CODE <- NULL; shp$GEOLEVEL1 <- NULL
+st_write(obj = shp, dsn = "Data", layer = "shapefile", driver = "ESRI Shapefile")
+
+
+
+
+#################################################################################################
 ####  GROUNDWATER STORAGE YEAR AVERAGE  #########################################################
 r <- raster::brick("Data_Raw/ISIMIP3a/cwatm_gswp3-w5e5_obsclim_histsoc_default_groundwstor_global_monthly_1901_2019.nc")
 
@@ -78,21 +92,6 @@ names(qry) <- paste0("qr", years)
 output_nc <- "Data/qry.nc"
 # Scrivi il RasterBrick in un file NetCDF
 writeRaster(qry, filename = output_nc, format = "CDF", overwrite = TRUE)
-
-
-
-
-#################################################################################################
-####  SHAPEFILE  ############################################################################
-shp_orig <- sf::read_sf("GW_Data/world_geolev1_2021/world_geolev1_2021.shp")
-shp <- sf::st_transform(shp_orig, sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")) 
-# Rimuovo le regioni che danno geometry error
-elementi <- c(34, 381, 2070, 419, 420, 2071, 643, 770, 868, 930, 2072, 1065, 
-              1105, 1162, 1542, 1548, 1578, 1824, 1968, 2073)
-shp<- shp[-elementi,]
-
-# Rimuovo le variabili che non mi servono
-shp$BPL_CODE <- NULL; shp$CNTRY_CODE <- NULL; shp$GEOLEVEL1 <- NULL
 
 
 
