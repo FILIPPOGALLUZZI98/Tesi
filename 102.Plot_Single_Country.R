26d15a2ab949c6a58053c99178048452fd26ef3a
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-27 11:56:04 +0100

    Update 102.Plot_Single_Country.R

--- 102.Plot_Single_Country.R
+++ 102.Plot_Single_Country.R
@@ -10,9 +10,9 @@ data_gw_events <- sf::read_sf(paste0(path, country,"_gw_events_", r,".shp"))
 events <-read.csv(paste0(path, country, "_events.csv"))
 ######################################################################################################
 
-# Plot mappa anno scelto
-data <- subset(data_gw_events, year == y)
-ggplot(data, aes(fill=value)) + 
+# Plot mappa GW anno scelto
+data_year <- subset(data_gw_events, year == y)
+ggplot(data_year, aes(fill=value)) + 
   geom_sf(col="black") +
   theme_bw() +
   labs(fill="gw storage") +
@@ -20,10 +20,10 @@ ggplot(data, aes(fill=value)) +
 
 ######################################################################################################
 
-# Plot serie temporali per regioni scelte
+# Plot serie temporali GW per regioni scelte
 # Selezionare le regioni per le serie temporali
-R <- c("Borno", "Gombe", "Nasarawa", "Sokoto")
-# R <- c(data_gw_events$ADMIN_NAME)  ## Se voglio vederle tutte insieme
+# R <- c("", "", "", "")
+R <- c(data_gw_events$ADMIN_NAME)  ## Se voglio vederle tutte insieme
 
 ggplot(subset(data_gw_events, ADMIN_NAME %in% R), 
        aes(year, value, fill=value, col=value)) +   
@@ -36,24 +36,43 @@ ggplot(subset(data_gw_events, ADMIN_NAME %in% R),
   scale_fill_viridis_c(option="viridis", end  = 0.8) +                              
   scale_color_viridis_c(option="viridis", end = 0.8)
 
-######################################################################################################
 
-data <- subset(data_gw_events, year == 2019)
-data <- subset(data_gw_events, type == "state")
-ggplot(data, aes(fill = number)) +  
-  # plots the sf object / shapefile
-  geom_sf() + 
-  # nice theme
-  theme_bw() +         
-  # viridis color palette
-  scale_fill_viridis_c(option="inferno", end=0.8) 
+######################################################################################################
+# Plot della timeseries in una regione dei conflitti Nstate
+
+reg <- "Kayin"
+data <- subset(data_gw_events,ADMIN_NAME==reg)
+data <- subset(data, type=="Nstate")
+
+
+ggplot(data = agg_data, aes(x = year, y = count)) +
+  geom_point() +  # Aggiunge i punti
+  geom_line() +   # Aggiunge la linea
+  labs(x = "Year", y = "Count") +
+  ggtitle("Somma dei valori 'number' per anno")
+
+
+######################################################################################################
+# Plot della timeseries dei conflitti in una regione state+Nstate+onesided
+
+reg <- "Kayin"
+data <- subset(data_gw_events,ADMIN_NAME==reg)
+agg_data <- data %>%
+  group_by(year, ADMIN_NAME) %>%
+  summarise(count = sum(number))
+
+ggplot(data = agg_data, aes(x = year, y = count)) +
+  geom_point() +  # Aggiunge i punti
+  geom_line() +   # Aggiunge la linea
+  labs(x = "Year", y = "Count") +
+  ggtitle("Somma dei valori 'number' per anno")
 
 ######################################################################################################
 # Da togliere state e mettere data_gw_events
 
 ggplot(data_gw_events) +           
   geom_sf(fill = NA, col = "black") +  
-  geom_point(data = data_gw_events$number, aes(longitude, latitude, color = factor(type)), size = .5) +
+  geom_point(data = events, aes(longitude, latitude, color = factor(type)), size = .5) +
   # Assegna manualmente i colori ai valori di code_status
   scale_color_manual(values = c("state" = "red", "Nstate" = "blue", "onesided" = "green"))+
   theme_bw() +                     
