6a0fb64275b759b858feafb9d6e3abf22a64e6a8
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-28 11:46:27 +0100

    Update 102.Plot_Single_Country.R

--- 102.Plot_Single_Country.R
+++ 102.Plot_Single_Country.R
@@ -1,8 +1,7 @@
-library(foreign); library(sf)
 # Select the country
-country <- "Ethiopia"
+country <- "Nigeria"
 # Select the year
-y <- "2000"
+y <- "2010"
 # Select the raster
 r <- "rs"
 
@@ -11,13 +10,13 @@ data_gw_events <- read.dbf(paste0(path, country,"_gw_events_", r,".dbf"))
 events <-read.csv(paste0(path, country, "_events.csv"))
 state <- subset(shp, CNTRY_NAME == country)    ## plot(state[,"geometry"])
 state <- rename(state, region = ADMIN_NAME)
+print(unique(data_gw_events$region))
 
 ######################################################################################################
 # Plot mappa GW anno scelto
 
 data_year <- subset(data_gw_events, year == y)
 data_year <- left_join(state, data_year, by = "region")
-
 ggplot(data_year, aes(fill=value)) + 
   geom_sf(col="black") +
   theme_bw() +
@@ -32,43 +31,34 @@ ggplot(data_year, aes(fill=value)) +
 R <- unique(data_gw_events$region)  ## Se voglio vederle tutte insieme
 
 ggplot(subset(data_gw_events, region %in% R), 
-       aes(year, value, fill=value, col=value)) +   
+       aes(year, value)) +   
   facet_wrap(region~., ncol=6) +        
-  theme_bw() +         
+  theme_minimal()+
   theme(strip.background=element_rect(fill="white")) +          
-  ylab("") +                                                                
-  xlab("") +                                                                       
-  geom_col() +                                                                      
-  scale_fill_viridis_c(option="viridis", end  = 0.8) +                              
-  scale_color_viridis_c(option="viridis", end = 0.8)
-
-
+  geom_line()
 
 ######################################################################################################
 
 # Plot data points over geometry figure
 ggplot() +
-  geom_sf(data = state) +  # Aggiunge le geometrie dello shapefile
-  geom_point(data = events, aes(x = longitude, y = latitude, color = type, size = number)) + # Aggiunge i punti degli eventi
+  geom_sf(data = state)+
+  geom_point(data = events, aes(x = longitude, y = latitude, color = type, size = number)) + 
   scale_color_discrete(name = "Type") +
-  # Assegna manualmente i colori ai valori di code_status
   scale_color_manual(values = c("state" = "blue", "Nstate" = "red", "onesided" = "green")) +
   labs(title = "Events") +
   theme_bw() +                     
-  # white background for variable names
   theme(strip.background = element_rect(fill="white"))
 
 
 ######################################################################################################
 # Plot della timeseries in una regione dei conflitti Nstate
 
-print(unique(data_gw_events$ADMIN_NAME))
 reg <- "Borno"
 
-data <- subset(data_gw_events,ADMIN_NAME==reg); data$geometry=NULL
+data <- subset(data_gw_events,region==reg)
 data <- subset(data, type=="Nstate")
 agg_data <- data %>%
-  group_by(year, ADMIN_NAME) %>%
+  group_by(year, region) %>%
   summarise(count = sum(number))
 
 ggplot(data = agg_data, aes(x = year, y = count)) +
@@ -82,20 +72,20 @@ ggplot(data = agg_data, aes(x = year, y = count)) +
 reg <- c("Abia", "Adamawa", "Anambra", "Borno", "Edo", "Jigawa", "Nasarawa", "Ogun")
 data <- data_gw_events; data$geometry=NULL
 agg_data <- data %>%
-  filter(type == "Nstate", ADMIN_NAME %in% reg) %>%
-  group_by(year, ADMIN_NAME) %>%
+  filter(type == "Nstate", region %in% reg) %>%
+  group_by(year, region) %>%
   summarise(count = sum(number))
 
 ggplot(data = agg_data, aes(x = year, y = count)) +
   geom_line() +   # Aggiunge la linea
   labs(x = "Year", y = "Count") +
-  facet_wrap(~ ADMIN_NAME, ncol = 3)
+  facet_wrap(~ region, ncol = 3)
 
 ######################################################################################################
 # Plot della timeseries dei conflitti in una regione state+Nstate+onesided
 
 reg <- "Borno"
-data <- subset(data_gw_events,ADMIN_NAME==reg); data$geometry=NULL
+data <- subset(data_gw_events,region==reg); data$geometry=NULL
 agg_data <- data %>%
   group_by(year) %>%
   summarise(count = sum(number))
@@ -109,25 +99,25 @@ ggplot(data = agg_data, aes(x = year, y = count)) +
 
 # reg <- unique(data_gw_events$ADMIN_NAME)  ## Se voglio vederle tutte insieme
 reg <- c("Abia", "Adamawa", "Anambra", "Borno", "Edo", "Jigawa", "Nasarawa", "Ogun")
-data <- data_gw_events; data$geometry=NULL
+data <- data_gw_events
 agg_data <- data %>%
-  filter(ADMIN_NAME %in% reg) %>%
-  group_by(year, ADMIN_NAME) %>%
+  filter(region %in% reg) %>%
+  group_by(year, region) %>%
   summarise(count = sum(number))
 
 
 ggplot(data = agg_data, aes(x = year, y = count)) +
   geom_line() +   # Aggiunge la linea
   labs(x = "Year", y = "Count") +
-  facet_wrap(~ ADMIN_NAME, ncol = 3)
+  facet_wrap(~ region, ncol = 3)
 
 ######################################################################################################
 # PLOT GW+CONFLICTS IN ONE REGION state+Nstate+onesided
 
 reg <- "Borno"
-data <- subset(data_gw_events,ADMIN_NAME==reg); data$geometry=NULL; data$CNTRY_NAME=NULL
+data <- subset(data_gw_events,region==reg); data$geometry=NULL; data$CNTRY_NAME=NULL
 agg_data <- data %>%
-  group_by(year, ADMIN_NAME, value) %>%
+  group_by(year, region, value) %>%
   summarise(count = sum(number))
 mvalue <- mean(agg_data$value)
 svalue <- sd(agg_data$value)
@@ -137,10 +127,6 @@ agg_data$Svalue <- (agg_data$value-mvalue)/svalue
 agg_data$Scount <- (agg_data$count-mcount)/scount
 
 ggplot(agg_data, aes(year)) +
-  geom_smooth(aes(y = Svalue), colour = "blue") +
-  geom_smooth(aes(y = Scount), colour = "red")
-
-ggplot(agg_data, aes(year)) +
   geom_line(aes(y = Svalue), colour = "blue") +
   geom_line(aes(y = Scount), colour = "red")
 
@@ -148,16 +134,17 @@ ggplot(agg_data, aes(year)) +
 # PLOT GW+CONFLICTS SLECTED REGIONS state+Nstate+onesided
 
 reg <- c("Abia", "Adamawa", "Anambra", "Borno", "Edo", "Jigawa", "Nasarawa", "Ogun")
-reg <- unique(data_gw_events$ADMIN_NAME)
-data <- data_gw_events; data$geometry=NULL
+reg <- unique(data_gw_events$region)
+data <- data_gw_events
+agg_data <- subset(data, region==reg)
 agg_data <- data %>%
-  filter(ADMIN_NAME %in% reg) %>%
-  group_by(year, ADMIN_NAME, value) %>%
+  filter(region %in% reg) %>%
+  group_by(year, region, value) %>%
   summarise(count = sum(number))
 
 b<-data.frame()
 for (i in reg){
-  a <- subset(agg_data, ADMIN_NAME==i)
+  a <- subset(agg_data, region==i)
   mvalue <- mean(a$value)
   svalue <- sd(a$value)
   mcount <- mean(a$count)
@@ -171,7 +158,7 @@ agg_data <- b
 ggplot(data = agg_data, aes(year)) +
   geom_line(aes(y = Svalue), colour = "blue") +
   geom_line(aes(y = Scount), colour = "red")+
-  facet_wrap(~ ADMIN_NAME, ncol = 3)
+  facet_wrap(~ region, ncol = 3)
 
 
 
