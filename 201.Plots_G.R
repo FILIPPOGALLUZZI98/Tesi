cf3e31537956abff8c275a8cbeac7755161e2599
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-04 14:58:00 +0200

    Update 201.Plots_G.R

--- 201.Plots_G.R
+++ 201.Plots_G.R
@@ -1,1 +1,12 @@
 
+
+dati_filtrati <- gw_g %>%
+  filter(year == 1920) 
+
+ggplot() +
+  geom_sf(data = shp, aes(fill = region)) +
+  geom_sf(data = dati_filtrati, aes(color = value), size = 2, show.legend = "point") +
+  scale_color_viridis_c() +  # Scala dei colori per i valori 'value'
+  scale_fill_manual(values = "transparent") +  # Trasparenza per le geometrie dello shapefile
+  labs(title = "Mappa del valore per regione (1920)",
+       color = "Value")
