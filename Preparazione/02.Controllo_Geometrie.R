0fd9537351fb17248745f66105891f9e53e84a8b
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-04 11:37:26 +0200

    Update 02.Controllo_Geometrie.R

--- Preparazione/02.Controllo_Geometrie.R
+++ Preparazione/02.Controllo_Geometrie.R
@@ -2,19 +2,6 @@ shp <- st_read("^Data/Shapefile/shp.gpkg")
 shp <- sf::st_transform(shp, sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
 r <- raster::brick("^Data/Raster/gws.nc"); proj4string(r) <- raster::crs(shp)
 
-state <- subset(shp, CNTRY_NAME == "France")
+state <- subset(shp, CNTRY_NAME == "Australia")
 plot(r$X1)
 plot(state, add = TRUE)
-
-
-
-
-
-
-
-
-
-
-
-
-
