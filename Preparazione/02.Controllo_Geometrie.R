502854fd04ec150c9fd68d5d118439b809b06ee0
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-04 11:36:24 +0200

    Update 02.Controllo_Geometrie.R

--- Preparazione/02.Controllo_Geometrie.R
+++ Preparazione/02.Controllo_Geometrie.R
@@ -1,16 +1,11 @@
-shp <- sf::read_sf("GW_Data/world_geolev1_2021/world_geolev1_2021.shp")
+shp <- st_read("^Data/Shapefile/shp.gpkg")
 shp <- sf::st_transform(shp, sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+r <- raster::brick("^Data/Raster/gws.nc"); proj4string(r) <- raster::crs(shp)
 
-# Groundwater Storage
-rs <- raster::brick("Data/GW.gwy.nc"); proj4string(rs) <- raster::crs(shp)
-# Total Water Storage
-rt <- raster::brick("Data/GW.twsy.nc"); proj4string(rt) <- raster::crs(shp)
-# Runoff (??)
-rq <- raster::brick("Data/GW.qry.nc"); proj4string(rq) <- raster::crs(shp)
-
-plot(rs$X1)
-plot(shp[,"geometry"], add=TRUE) 
+state <- subset(shp, CNTRY_NAME == "France")
+plot(r$X1)
 plot(state, add = TRUE)
+
 
 
 
