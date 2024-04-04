e47594cc9a091631a8e4b9798208071cd7107139
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-04 11:45:28 +0200

    Update 101.Datasets_Single_Country.R

--- Preparazione/101.Datasets_Single_Country.R
+++ Preparazione/101.Datasets_Single_Country.R
@@ -1,14 +1,11 @@
 # Select the country
 country <- "Brazil"
 # Select the raster
-rast <- "gws"
-
-
+rast <- "gws"    ## gws; ...
 
 
 ##############################################################################################################################
 ##############################################################################################################################
-
 suppressPackageStartupMessages({
   library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
   library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign)})
