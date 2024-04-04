a6245d0f669969a894d22d72a70a39ecf893298e
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-04 13:19:14 +0200

    Update 101.Datasets_Single_Country.R

--- Preparazione/101.Datasets_Single_Country.R
+++ Preparazione/101.Datasets_Single_Country.R
@@ -24,9 +24,7 @@ if (!file.exists(percorso_cartella)) {
 ##############################################################################################################################
 ##############################################################################################################################
 
-# Media dei valori del raster sulle regioni 
 gw_sc <- exactextractr::exact_extract(r, state, fun="mean")
-# Aggiungo una colonna region al file gw_data_state
 gw_sc$region <- state$region
 gw_sc <- reshape2::melt(gw_sc, id.vars="region")
 anni <- 1901:2019
@@ -42,9 +40,8 @@ write.csv(gw_sc, paste0(percorso_cartella, paese, "_",rast, ".csv"))
 ####  POINT DATA CONFLICT UPPSALA  ##########################################################################################
 # DA OTTIMIZZARE, RIMUOVERE IL SALVATAGGIO INTERMEDIO CHE è INUTILE
 
-file_path <- paste("Data_Raw/Conflict_Data/", country, ".csv", sep = "")
+file_path <- paste("^Data_Raw/Conflict_Data/", paese, ".csv", sep = "")
 events <- read.csv(file_path)
-# Seleziono soltanto le variabili che mi interessano
 events <- events[, c("relid", "code_status","latitude" ,"longitude", "best_est")]
 events <- events %>%
   rename(year = relid,
@@ -56,17 +53,6 @@ events <- mutate(events,
                    type == 3 ~ "onesided"
                  ))
 
-output_folder <- "Data/Conflict"
-output_file <- file.path(output_folder, paste0(country, ".csv"))
-write.csv(events, file = output_file, row.names = FALSE)
-
-##############################################################################################################################
-####  EVENTS INTERSECTION  ####################################################################################################
-
-
-# Operazioni sui dati events
-file_path <- paste("Data/Conflict/", country, ".csv", sep = "")
-events <- read.csv(file_path)
 events$latitude=NULL
 events$latitude <- as.numeric(str_extract(events$longitude, "(?<=POINT \\()[0-9.-]+"))
 events$longitude <- as.numeric(str_extract(events$longitude, "(?<=\\s)[0-9.-]+(?=\\))"))
@@ -75,21 +61,25 @@ events = sf::st_as_sf(events, coords = c("latitude","longitude"), remove = FALSE
 sf::st_crs(events) = sf::st_crs(state)
 events$longitude <- st_coordinates(events)[, "X"]
 events$latitude <- st_coordinates(events)[, "Y"]
+
 intersection = sf::st_intersects(events, state)            
 # set non-matched values to NA, these points are recorded in no province (outliers, miscoding, ...)
 intersection[sapply(intersection, length) == 0] <- NA    
 # merge region name to conflict data points
-events$region <- state$ADMIN_NAME[unlist(intersection)]   
+events$region <- state$region[unlist(intersection)]   
 events$geometry = NULL
 events <- events %>% group_by(year, region, type, latitude, longitude) %>% summarise(number_events = n())
 colnames(events) <- c("year","region", "type", "latitude", "longitude","number")
 # Ordino il dataset rispetto all'anno
 events <- events[order(events$year),]
 
+# Salvataggio dati
+write.csv(events,paste0(percorso_cartella, paese, "_events.csv"))
+
 ##############################################################################################################################
 ##############################################################################################################################
 # Operazioni sui dati GW + events
-gw_data_sc <- gw_data_sc %>%
+gw_data_sc <- gw_sc %>%
   filter(year > 1989)
 
 gw_events_sc <- expand.grid(year = 1990:2022, region = unique(events$region),type=c("state","Nstate","onesided"))
@@ -108,8 +98,8 @@ gw_events_sc <- left_join(gw_events_sc, gw_data_sc[,c("year", "region","value")]
 ##############################################################################################################################
 # Scrittura datasets
 
-write.csv(gw_events_sc, paste0(percorso_cartella, country, "_gw_events_",rast, ".csv"))
-write.csv(events,paste0(percorso_cartella, country, "_events.csv"))
+write.csv(gw_events_sc, paste0(percorso_cartella, paese, "_", rast, "_events", ".csv"))
+
 
 
 
