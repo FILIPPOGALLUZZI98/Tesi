105a2894e974ece7235c0035e87298da95ce791e
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-04 11:43:30 +0200

    Update 01.Data_Prep.R

--- Preparazione/01.Data_Prep.R
+++ Preparazione/01.Data_Prep.R
@@ -1,3 +1,7 @@
+suppressPackageStartupMessages({
+  library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
+  library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign)})
+
 #################################################################################################
 ####  SHAPEFILE  ############################################################################
 
