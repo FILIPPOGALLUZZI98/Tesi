a8a0cb310bcaf1db12fb36d88ca220c480a26931
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-04 14:33:20 +0200

    Update 201.Datasets_G.R

--- Preparazione/201.Datasets_G.R
+++ Preparazione/201.Datasets_G.R
@@ -1,47 +1,47 @@
-# Scegliere quale raster usare (rs, rt, rq)
-rast <- "rs"
+# Select the raster (gws, ...)
+rast <- "gws"
 
+##############################################################################################################################
+##############################################################################################################################
 suppressPackageStartupMessages({
-  library(sf)              ## useful for spatial manipulations
-  library(sp)              ## useful for spatial manipulations
-  library(plyr )
-  library(raster)          ## useful for working with raster data
-  library(ncdf4)           ## useful for working with raster data
-  library(exactextractr)   ## useful for extracting data from raster files
-  library(dplyr)           ## useful for merging data sets
-  library(stringr)
-  library(reshape2)        ## useful for manipulating data sets
-  library(ggplot2)         ## useful for data visualization
-  library(ggrepel)         ## useful for labeling point plots in ggplot2
-  library(lubridate)
-  library(zoo)   
-  library(foreign)
-})
+  library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
+  library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign)})
 
-shp <- sf::read_sf("Data/Shapefile/shapefile.shp")
-shp <- sf::st_transform(shp, sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
-r <- raster::brick(paste0("Data/GW/",rast,"y.nc")); proj4string(r) <- raster::crs(shp)
-
-##############################################################################################
-##############################################################################################
-
-
-gw_data_g <- exactextractr::exact_extract(r, shp, fun="mean")
-gw_data_g$region <- shp$ADMIN_NAME ; gw_data_g$country <- shp$CNTRY_NAME
-gw_data_g <- reshape2::melt(gw_data_g, id.vars=c("country", "region"))
-gw_data_g$region <- ifelse(is.na(gw_data_g$region), gw_data_g$country, gw_data_g$region)
-gw_data_g$variable <- gsub("mean.X", "", gw_data_g$variable)  # Rimuovi "mean.X"
-gw_data_g$variable <- as.integer(gsub("\\D", "", gw_data_g$variable)) + 1900 
+# Select shapefile and raster
+shp <- st_read("^Data/shp.gpkg")
+r <- raster::brick(paste0("^Data/",rast,".nc"))
 
 
 ##############################################################################################
+####  GLOBAL GW  #############################################################################
+
+# Merging data gw-shapefile
+gw_g <- exactextractr::exact_extract(r, shp, fun="mean")
+
+# Add columns for regions and countries
+gw_g$region <- shp$region ; gw_g$country <- shp$country
+
+# Reshape the dataset into a long form
+gw_g <- reshape2::melt(gw_g, id.vars=c("country", "region"))
+
+# Rename the years
+gw_g$variable <- gsub("mean.X", "", gw_g$variable)  # Rimuovi "mean.X"
+gw_g$variable <- as.integer(gsub("\\D", "", gw_g$variable)) + 1900 
+
+# Save data
+write.csv(gw_g, paste0("^Data/", "Global_",rast, ".csv"), row.names=FALSE)
+
 ##############################################################################################
+####  POINT DATA CONFLICT UPPSALA  ###########################################################
 
-
-file_path <- "Data_Raw/Conflict_Data/Global.csv"
+# Select the conflict data
+file_path <- "^Data_Raw/Conflict_Data/Global.csv"
 events <- read.csv(file_path)
+
+# Select the variables of interest
 events <- events[, c("country" ,"year", "type_of_violence","latitude" ,"longitude", "best")]
 
+# Rename the variables
 events <- events %>%
   rename(type = type_of_violence,
          number = best)
@@ -51,16 +51,17 @@ events <- mutate(events,
                    type == 2 ~ "Nstate",
                    type == 3 ~ "onesided"
                  ))
+# Set the coordinate system
 events <- na.omit(events[, c("country" ,"year", "type","latitude" ,"longitude","number")])
 events <- sf::st_as_sf(events, coords = c("latitude","longitude"), remove = FALSE)
 sf::st_crs(events) <- sf::st_crs(shp)
+events$longitude <- st_coordinates(events)[, "X"]
+events$latitude <- st_coordinates(events)[, "Y"]
 intersection <- sf::st_intersects(events, shp)            
 intersection[sapply(intersection, length) == 0] <- NA    
 
 
 
-##############################################################################################
-##############################################################################################
 
 
 
