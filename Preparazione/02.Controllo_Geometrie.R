952502381702154de683961bf78a0bd5963d0638
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-04 11:44:01 +0200

    Update 02.Controllo_Geometrie.R

--- Preparazione/02.Controllo_Geometrie.R
+++ Preparazione/02.Controllo_Geometrie.R
@@ -1,3 +1,10 @@
+suppressPackageStartupMessages({
+  library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
+  library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign)})
+
+############################################################################################################################
+############################################################################################################################
+
 shp <- st_read("^Data/Shapefile/shp.gpkg")
 shp <- sf::st_transform(shp, sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
 r <- raster::brick("^Data/Raster/gws.nc"); proj4string(r) <- raster::crs(shp)
