acaebf1f7798138568908e05d52b4b229a0543c9
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-04 11:31:24 +0200

    Update 03.elementi_rimossi.R

--- Preparazione/03.elementi_rimossi.R
+++ Preparazione/03.elementi_rimossi.R
@@ -1,19 +1,3 @@
-nomi_rimossi
-elementi <- c(25, 15, 12, c(5, 6), 27, 19, 10, 11, 9, 13, 14, 39, 14, 13, 5, 20, 38, 52, 20)
-
-
-state <- subset(shp, CNTRY_NAME == "United States")
-state <- state[-elementi[18],]
-
-for (i in seq_along(nomi_rimossi)){
-  state <- subset(shp, CNTRY_NAME == nomi_rimossi[i])
-  state <- state[-elementi[i], ]
-  
-}
-
-plot(state[,"geometry"])  
-
-
 # elementi geometry empty
 argentina 25
 ecuador 15
