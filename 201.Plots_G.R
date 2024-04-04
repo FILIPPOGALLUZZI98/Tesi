7e17cd1d912a8f11861d8ea65d03b0291c3890e9
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-04 16:15:37 +0200

    Update 201.Plots_G.R

--- 201.Plots_G.R
+++ 201.Plots_G.R
@@ -1,12 +1,38 @@
+suppressPackageStartupMessages({
+  library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
+  library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign)})
+
+shp <- st_read("^Data/shp.gpkg")
+events <-read.csv("^Data/Global_events.csv")
+data_gw <- read.csv("^Data/Global_gws.csv")
+##  data_gw_events <- read.csv("^Data/Global_gws_events.csv")
 
 
-dati_filtrati <- gw_g %>%
-  filter(year == 1920) 
+######################################################################################################
+# Plot GW map for a selected year and country
+y="2002"; paese="Nigeria"
 
-ggplot() +
-  geom_sf(data = shp, aes(fill = region)) +
-  geom_sf(data = dati_filtrati, aes(color = value), size = 2, show.legend = "point") +
-  scale_color_viridis_c() +  # Scala dei colori per i valori 'value'
-  scale_fill_manual(values = "transparent") +  # Trasparenza per le geometrie dello shapefile
-  labs(title = "Mappa del valore per regione (1920)",
-       color = "Value")
+data <- subset(data_gw, year == y & country == paese)
+state <- subset(shp, country==paese)
+plot_mappa <- ggplot() +
+  geom_sf(data = state, aes(fill = region), color = "black", size = 0.2) +
+  geom_sf(data = data, aes(fill = value), color = "black", size = 0.2) +
+  scale_fill_viridis_c() +
+  labs(title = paste("Mappa del valore per regione in", paese, "nel", y),
+       fill = "Value")
+
+# Visualizza il plot
+print(plot_mappa)
+
+
+
+
+ggplot(data, aes(fill=value)) + 
+  geom_sf(col="black") +
+  theme_bw() +
+  labs(fill="GW storage [kg/m^2]") +
+  scale_fill_viridis_c(option="viridis", end=0.8)+
+  labs(title = paste(paese, y))
+
+
+
