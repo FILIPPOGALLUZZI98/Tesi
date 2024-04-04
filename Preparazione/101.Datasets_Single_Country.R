87f5f6afb9e45b38cbc7818c10f11742c6f9e188
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-04 13:37:33 +0200

    Update 101.Datasets_Single_Country.R

--- Preparazione/101.Datasets_Single_Country.R
+++ Preparazione/101.Datasets_Single_Country.R
@@ -1,5 +1,5 @@
 # Select the country
-paese <- "Brazil"
+paese <- "Ethiopia"
 # Select the raster
 rast <- "gws"    ## gws; ...
 
@@ -10,39 +10,52 @@ suppressPackageStartupMessages({
   library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
   library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign)})
 
+# Select shapefile and raster
 shp <- st_read("^Data/shp.gpkg")
-shp <- sf::st_transform(shp, sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
-r <- raster::brick(paste0("^Data/",rast,".nc")); proj4string(r) <- raster::crs(shp)
-# Subset dello shapefile per il paese selezionato
+r <- raster::brick(paste0("^Data/",rast,".nc"))
+
+# Subset of the shapefile for the selected country
 state <- subset(shp, country == paese)    ## plot(state[,"geom"])
 
+# Create a directory for datasets
 percorso_cartella <- paste0("^Data/Single_Country/",paese,"/")
 if (!file.exists(percorso_cartella)) {
   dir.create(percorso_cartella, recursive = TRUE)
 }
 
 ##############################################################################################################################
-##############################################################################################################################
+####  GW DATA  ###############################################################################################################
 
+# Merging data gw-shapefile
 gw_sc <- exactextractr::exact_extract(r, state, fun="mean")
+
+# I add a column for the regions
 gw_sc$region <- state$region
+
+# I reshape the dataset into a long form
 gw_sc <- reshape2::melt(gw_sc, id.vars="region")
+
+# I rename the years
 anni <- 1901:2019
 gw_sc <- gw_sc %>%
   group_by(region) %>%
   mutate(year = anni)
 gw_sc$variable=NULL
 
-# Salvataggio dati
-write.csv(gw_sc, paste0(percorso_cartella, paese, "_",rast, ".csv"))
+# Save data
+write.csv(gw_sc, paste0(percorso_cartella, paese, "_",rast, ".csv"), row.names=FALSE)
 
 #############################################################################################################################
 ####  POINT DATA CONFLICT UPPSALA  ##########################################################################################
-# DA OTTIMIZZARE, RIMUOVERE IL SALVATAGGIO INTERMEDIO CHE è INUTILE
 
+# Select the conflict data
 file_path <- paste("^Data_Raw/Conflict_Data/", paese, ".csv", sep = "")
 events <- read.csv(file_path)
+
+# Select the variables of interest
 events <- events[, c("relid", "code_status","latitude" ,"longitude", "best_est")]
+
+# Rename the variables
 events <- events %>%
   rename(year = relid,
          type = code_status)
@@ -53,6 +66,7 @@ events <- mutate(events,
                    type == 3 ~ "onesided"
                  ))
 
+# Set the coordinate system
 events$latitude=NULL
 events$latitude <- as.numeric(str_extract(events$longitude, "(?<=POINT \\()[0-9.-]+"))
 events$longitude <- as.numeric(str_extract(events$longitude, "(?<=\\s)[0-9.-]+(?=\\))"))
@@ -62,47 +76,47 @@ sf::st_crs(events) = sf::st_crs(state)
 events$longitude <- st_coordinates(events)[, "X"]
 events$latitude <- st_coordinates(events)[, "Y"]
 
+# Intersection shapefile-events
 intersection = sf::st_intersects(events, state)            
-# set non-matched values to NA, these points are recorded in no province (outliers, miscoding, ...)
+
+# Set non-matched values to NA, these points are recorded in no province (outliers, miscoding, ...)
 intersection[sapply(intersection, length) == 0] <- NA    
-# merge region name to conflict data points
+
+# Merge region name to conflict data points
 events$region <- state$region[unlist(intersection)]   
 events$geometry = NULL
+
+# Group events for the same region and type in one year 
 events <- events %>% group_by(year, region, type, latitude, longitude) %>% summarise(number_events = n())
+
+# Rename the variables names
 colnames(events) <- c("year","region", "type", "latitude", "longitude","number")
-# Ordino il dataset rispetto all'anno
+
+# Sort datasets by year
 events <- events[order(events$year),]
 
-# Salvataggio dati
-write.csv(events,paste0(percorso_cartella, paese, "_events.csv"))
+# Save data
+write.csv(events,paste0(percorso_cartella, paese, "_events.csv"), row.names=FALSE)
 
 ##############################################################################################################################
 ##############################################################################################################################
-# Operazioni sui dati GW + events
+
+# Select for GW data the same timespan as for conflicts
+# Create a dataset in which I have for each year and for each region the value of GW, the number of
+# conflicts (that could be 0 if there were none) and the type of conflict
+
 gw_data_sc <- gw_sc %>%
   filter(year > 1989)
-
-gw_events_sc <- expand.grid(year = 1990:2022, region = unique(events$region),type=c("state","Nstate","onesided"))
+gw_events_sc <- expand.grid(year = 1989:2022, region = unique(events$region),type=c("state","Nstate","onesided"))
 gw_events_sc <- left_join(gw_events_sc, events, by=c("region", "year", "type"="type"))
-gw_events_sc$number[is.na(gw_events_sc$number)] = 0 # assign a zero to each month/province where no data is observed
+gw_events_sc$number[is.na(gw_events_sc$number)] = 0  ## Assign a zero to each month/province where no data is observed
 gw_events_sc$latitude = NULL ; gw_events_sc$longitude=NULL
 gw_events_sc <- gw_events_sc %>%
   filter(year != 2020 & year != 2021 & year != 2022)
 gw_events_sc <- left_join(gw_events_sc, gw_data_sc[,c("year", "region","value")], by=c("year", "region"))
-# st_geometry(gw_events_sc) <- "geometry"
 
-
-
-
-##############################################################################################################################
-##############################################################################################################################
-# Scrittura datasets
-
-write.csv(gw_events_sc, paste0(percorso_cartella, paese, "_", rast, "_events", ".csv"))
-
-
-
-
+# Salvataggio dati
+write.csv(gw_events_sc, paste0(percorso_cartella, paese, "_", rast, "_events", ".csv"), row.names=FALSE)
 
 
 
