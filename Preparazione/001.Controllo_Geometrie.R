9f31a0f340e36552040b13424d78fce5b625129b
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-05 16:14:44 +0200

    Update 001.Controllo_Geometrie.R

--- Preparazione/001.Controllo_Geometrie.R
+++ Preparazione/001.Controllo_Geometrie.R
@@ -1,3 +1,5 @@
+# This is the code to check if the geometries of the shapefile and the raster coincide
+
 suppressPackageStartupMessages({
   library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
   library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign)})
