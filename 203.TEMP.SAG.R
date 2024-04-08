5a3db8a3696e899e0f44c92846039771b8dcf232
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-08 09:40:42 +0200

    Create 203.TEMP.SAG.R

---
+++ 203.TEMP.SAG.R
@@ -0,0 +1,25 @@
+suppressPackageStartupMessages({
+  library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
+  library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign)})
+
+shp <- st_read("^Data/shp/shp.shp")
+events <- read.csv("^Data/Global_events.csv")
+data_gw <- read.csv("^Data/Global_gws.csv")
+data_gw_events <-read.csv("^Data/Global_events_gws.csv")
+
+
+data <- data_gw_events %>%
+  group_by(country, region, year, value, type) %>%
+  summarise(count = sum(number_deaths))
+
+lm <- lm(log(1+data$count) ~ data$value + as.factor(data$year) + as.factor(data$region))
+summary(lm)
+plot(data$count ~ data$value )
+
+fixest::feols(data=data, log(1+count)~value|region + year)
+
+fixest::feglm(data=data, count~value|region + year, family=quasipoisson)
+
+fixest::feglm(data=subset(data, type=="Nstate"), count~value|region + year, family=quasipoisson)
+
+              
