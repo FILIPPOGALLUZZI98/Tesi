972c5bebc19bc7222fca363a8ccd378b56669d20
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-08 10:50:16 +0200

    Update 103.G_Statistical_Analysis.R

--- 103.G_Statistical_Analysis.R
+++ 103.G_Statistical_Analysis.R
@@ -6,9 +6,11 @@ shp <- st_read("^Data/shp/shp.shp")
 deaths <- read.csv("^Data/Global_deaths.csv")
 conflicts <- read.csv("^Data/Global_conflicts.csv")
 data_gw <- read.csv("^Data/Global_gws.csv")
-data_gw_deaths <-read.csv("^Data/Global_deaths_gws.csv")
-data_gw_conflicts <-read.csv("^Data/Global_conflicts_gws.csv")
+data_deaths <-read.csv("^Data/Global_deaths_gws.csv")
+data_conflicts <-read.csv("^Data/Global_conflicts_gws.csv")
 
+##############################################################################################################################
+####  TEMP  ########################################################################################################################
 
 data <- data_gw_deaths %>%
   group_by(country, region, year, value, type) %>%
@@ -19,11 +21,39 @@ summary(lm)
 plot(data$count ~ data$value )
 
 
+##############################################################################################################################
+####  DEATHS  ########################################################################################################################
 
-fixest::feols(data=data, log(1+count)~value|region + year)
+# All the data, not divided by type of conflict
+fixest::feols(data=data_deaths, log(1+count)~value|region + year)
+fixest::feglm(data=data_deaths, count~value|region + year, family=quasipoisson)
 
-fixest::feglm(data=data, count~value|region + year, family=quasipoisson)
 
-fixest::feglm(data=subset(data, type=="Nstate"), count~value|region + year, family=quasipoisson)
+fixest::feglm(data=subset(data_deaths, type=="Nstate"), count~value|region + year, family=quasipoisson)
 
-              
+##############################################################################################################################
+####  CONFLICTS  ########################################################################################################################
+
+# All the data, not divided by type of conflict
+fixest::feols(data=data_conflicts, log(1+conflicts)~value|region + year)
+fixest::feglm(data=data_conflicts, log(1+conflicts)~value|region + year, family=quasipoisson)
+
+
+fixest::feglm(data=subset(data_deaths, type=="Nstate"), count~value|region + year, family=quasipoisson)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
