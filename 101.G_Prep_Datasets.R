58f9eb8d9d3c041cd3119af452acb3d9961a9294
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-08 10:30:59 +0200

    Update 101.G_Prep_Datasets.R

--- 101.G_Prep_Datasets.R
+++ 101.G_Prep_Datasets.R
@@ -46,7 +46,7 @@ gw_g <- gw_g[, c("year","country", "region", "value")]
 write.csv(gw_g, paste0("^Data/", "Global_",rast, ".csv"), row.names=FALSE)
 
 ##############################################################################################
-####  GLOBAL CONFLICT UPPSALA  ###############################################################
+####  GLOBAL CONFLICT UPPSALA - N_DEATHS  ####################################################
 
 # Select the conflict data
 file_path <- "^Data_Raw/Conflict_Data/Global.csv"
@@ -73,11 +73,13 @@ events <- st_transform(events, st_crs(shp))
 # Remove the invalid geometries from shp
 shp <- shp[st_is_valid(shp), ]
 
+
 # Intersection shapefile-events and aggregate data 
 events_joined <- st_join(events, shp)
 events_joined$country.x=NULL
 events_joined <- events_joined %>%
   rename(country = country.y)
+
 events <- aggregate(number_deaths ~ year + country + region + type, data = events_joined, sum)
 
 # Sort datasets by year
@@ -85,15 +87,61 @@ events <- events[order(events$country),]
 events <- events[order(events$year),]
 
 # Save data
-write.csv(events, paste0("^Data/", "Global_events", ".csv"), row.names=FALSE)
+write.csv(events, paste0("^Data/", "Global_deaths", ".csv"), row.names=FALSE)
+
+#############################################################################################################################
+####  GLOBAL CONFLICT UPPSALA - N_CONFLICTS  ################################################################################
+
+# Select the conflict data
+file_path <- "^Data_Raw/Conflict_Data/Global.csv"
+events <- read.csv(file_path)
+
+# Select the variables of interest
+events <- events[, c("country" ,"year", "type_of_violence","latitude" ,"longitude", "best")]
+
+# Rename the variables
+events <- events %>%
+  rename(type = type_of_violence)
+events <- mutate(events,
+                 type = case_when(
+                   type == 1 ~ "state",
+                   type == 2 ~ "Nstate",
+                   type == 3 ~ "onesided"
+                 ))
+
+# Set the coordinate system
+events <- st_as_sf(events, coords = c("longitude", "latitude"), crs = st_crs(shp))
+events <- st_transform(events, st_crs(shp))
+
+# Remove the invalid geometries from shp
+shp <- shp[st_is_valid(shp), ]
+
+
+# Intersection shapefile-events and aggregate data 
+events_joined <- st_join(events, shp)
+events_joined$country.x=NULL
+events_joined <- events_joined %>%
+  rename(country = country.y)
+
+events_joined$geometry=NULL
+events <- events_joined %>%
+  group_by(country, region, year, type) %>%
+  summarise(count = n())
+
+# Rename variables
+events <- events %>%
+  rename(conflicts = count)
+events <- events[, c("year","country", "region","type","conflicts")]
+
+# Save data
+write.csv(events, paste0("^Data/", "Global_conflicts", ".csv"), row.names=FALSE)
 
 #############################################################################################################################
 ####  GLOBAL MIGRATION DATASET  #############################################################################################
 
 
-
 ##############################################################################################################################
-####  GLOBAL JOINT DATASET GW-CONFLITC  ######################################################################################
+####  GLOBAL JOINT DATASET GW-DEATHS  ######################################################################################
 
 gw_data_g <- gw_g %>%
   filter(year > 1988)
@@ -107,10 +155,25 @@ gw_events_g$number_deaths[is.na(gw_events_g$number_deaths)] = 0  ## Assign a zer
 gw_events_g <- gw_events_g[, c("year","country", "region","type","number_deaths", "value")]
 
 # Save data
-write.csv(gw_events_g, paste0("^Data/", "Global_events_gws", ".csv"), row.names=FALSE)
+write.csv(gw_events_g, paste0("^Data/", "Global_deaths_gws", ".csv"), row.names=FALSE)
 
 
+##############################################################################################################################
+####  GLOBAL JOINT DATASET GW-CONFLITCS  #####################################################################################
 
+gw_data_g <- gw_g %>%
+  filter(year > 1988)
+events <- events %>%
+  filter(year<2020)
+
+vettore <- expand.grid(year=1989:2019, type=c("state","Nstate","onesided"))
+gw_events_g <- left_join(gw_data_g, vettore, by=c("year"))
+gw_events_g <- left_join(gw_events_g,events,by=c("country","region","year","type"))
+gw_events_g$conflicts[is.na(gw_events_g$conflicts)] = 0  ## Assign a zero to each month/province where no data is observed
+gw_events_g <- gw_events_g[, c("year","country", "region","type","conflicts", "value")]
+
+# Save data
+write.csv(gw_events_g, paste0("^Data/", "Global_conflicts_gws", ".csv"), row.names=FALSE)
 
 
 
