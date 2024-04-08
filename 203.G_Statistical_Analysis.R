678dd8a851c34cc0b66fdf51a4a283693cea4cd2
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-08 09:56:21 +0200

    Update 203.G_Statistical_Analysis.R

--- 203.G_Statistical_Analysis.R
+++ 203.G_Statistical_Analysis.R
@@ -3,16 +3,25 @@ suppressPackageStartupMessages({
   library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign)})
 
 shp <- st_read("^Data/shp/shp.shp")
-events <- read.csv("^Data/Global_events.csv")
+deaths <- read.csv("^Data/Global_deaths.csv")
+conflicts <- read.csv("^Data/Global_conflicts.csv")
 data_gw <- read.csv("^Data/Global_gws.csv")
-data_gw_events <-read.csv("^Data/Global_events_gws.csv")
+data_gw_deaths <-read.csv("^Data/Global_deaths_gws.csv")
+data_gw_conflicts <-read.csv("^Data/Global_conflicts_gws.csv")
+
+data <- data_gw_deaths %>%
+  group_by(country, region, year, value, type) %>%
+  summarise(count = sum(number_deaths))
+
+lm <- lm(log(1+data$count) ~ data$value + as.factor(data$year) + as.factor(data$region))
+summary(lm)
+plot(data$count ~ data$value )
+
+fixest::feols(data=data, log(1+count)~value|region + year)
+
+fixest::feglm(data=data, count~value|region + year, family=quasipoisson)
 
 
-data <- data_gw_events %>%
-  group_by(country, region, year, value) %>%
-  summarise(conflict = sum(number_deaths))
+fixest::feglm(data=subset(data, type=="Nstate"), count~value|region + year, family=quasipoisson)
 
-lm <- lm(data$conflict ~ data$value + as.factor(data$year) + as.factor(data$region))
-# summary(lm)
-plot(data$conflict ~ data$value)
-abline(lm)
+              
