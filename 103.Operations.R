35ba7b607ac7a14db987e8feaeaa69e1dcbec838
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-09 15:25:49 +0200

    Update 103.Operations.R

--- 103.Operations.R
+++ 103.Operations.R
@@ -1,3 +1,37 @@
+suppressPackageStartupMessages({
+  library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
+  library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign); library(countrycode);
+  library(fixest); library(broom);library(knitr)} )
+
+ge <- read.csv("^Data/gws_events.csv")
+gem <- read.csv("^Data/gws_migr_events.csv")
+
+#### 31 : (190929/3) = 58 : 119074 ####
+
+#################################################################################################
+#################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
