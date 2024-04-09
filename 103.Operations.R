bc21727f32e9a2c7edebc43be201309d8c71354d
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-09 16:00:43 +0200

    Update 103.Operations.R

--- 103.Operations.R
+++ 103.Operations.R
@@ -11,19 +11,27 @@ gem <- read.csv("^Data/gws_migr_events.csv")
 #################################################################################################
 #################################################################################################
 
-# Per ogni 'orig', per ogni 'type ,per ogni 'interval =1', per ogni 'year': media di 'value' con l'anno precedente e mettere in 'mvalue1'
-# Per ogni 'orig', per ogni 'type, per ogni 'interval =5', per ogni 'year': media di 'value' con i 5 anni precedenti e mettere in 'mvalue5'
-# Per ogni 'orig', per ogni 'type, per ogni 'interval =1', per ogni 'year': media di 'conflicts' con l'anno precedente e mettere in 'mconfl1'
-# Per ogni 'orig', per ogni 'type, per ogni 'interval =5', per ogni 'year': media di 'conflicts' con i 5 anni precedenti e mettere in 'mconfl5'
+# Per ogni 'orig', per ogni 'type, per ogni 'year': media di 'value' con l'anno precedente e mettere in 'mvalue1'
+# Per ogni 'orig', per ogni 'type, per ogni 'year': media di 'value' con i 5 anni precedenti e mettere in 'mvalue5'
+# Per ogni 'orig', per ogni 'type, per ogni 'year': media di 'conflicts' con l'anno precedente e mettere in 'mconfl1'
+# Per ogni 'orig', per ogni 'type, per ogni 'year': media di 'conflicts' con i 5 anni precedenti e mettere in 'mconfl5'
 
 
 
+gem <- gem %>%
+  arrange(type, orig, year) %>%
+  group_by(orig, type) %>%
+  mutate(mvalue1 = (value + lag(value, default = 0)) / 2)
 
+gem <- gem %>%
+  arrange(type, orig, year) %>%
+  group_by(orig, type) %>%
+  mutate(mvalue1 = (value + lag(value, default = 0)) / 2)
 
-
-
-
-
+gem <- gem %>%
+  arrange(type, orig, year) %>%
+  group_by(orig, type) %>%
+  mutate(mvalue1 = rollmean(value, k = 5, fill = NA, align = "right", na.pad = TRUE))
 
 
 
