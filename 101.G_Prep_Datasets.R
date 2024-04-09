e6d5fb4b755b651171cc5d480873565655151ca3
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-09 10:34:44 +0200

    Update 101.G_Prep_Datasets.R

--- 101.G_Prep_Datasets.R
+++ 101.G_Prep_Datasets.R
@@ -26,7 +26,6 @@ suppressPackageStartupMessages({
 shp <- st_read("^Data/shp/shp.shp")
 r <- raster::brick(paste0("^Data/",rast,".nc"))
 
-
 ##############################################################################################
 ####  GLOBAL GW  #############################################################################
 
@@ -124,10 +123,6 @@ write.csv(gw_events, paste0("^Data/", "Global_gws_events", ".csv"), row.names=FA
 
 #############################################################################################################################
 ####  GLOBAL MIGRATION DATASET  #############################################################################################
-
-shpm <- sf::read_sf("^Data_Raw/world_geolev1_2021/world_geolev1_2021.shp")
-shpm$BPL_CODE<-NULL; shpm$CNTRY_CODE<-NULL
-
 
 data_migr <-read.csv("^Data_Raw/Global_migr_raw.csv")
 # Sort the order of the variables
