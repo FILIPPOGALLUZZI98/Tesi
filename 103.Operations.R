9ecec923e94a6fd91aebf3aac76b021e44af7b39
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-09 16:34:05 +0200

    Update 103.Operations.R

--- 103.Operations.R
+++ 103.Operations.R
@@ -9,6 +9,9 @@ gem <- read.csv("^Data/gws_migr_events.csv")
 #################################################################################################
 #################################################################################################
 
+#### CONTROLLARE BENE!!
+#### SEMBRA SBAGLIATO
+
 # Mean value 1-year
 gem <- gem %>%
   arrange(type, orig, year) %>%
@@ -29,11 +32,47 @@ gem <- gem %>%
   arrange(type, orig, year) %>%
   group_by(orig, type) %>%
   mutate(vvalue1 = (value - lag(value, default = 0)))
-# Mean conflicts 1-year
+# Variation conflicts 1-year
 gem <- gem %>%
   arrange(type, orig, year) %>%
   group_by(orig, type) %>%
   mutate(vconfl1 = (conflicts - lag(conflicts, default = 0)))
+
+# Mean value 5-year
+gem <- gem[order(gem$orig, gem$type, gem$year), ]
+gem <- gem %>%
+  group_by(orig, type) %>%
+  mutate(mvalue5 = rollmean(value, k = 5, align = "right", fill = NA))
+# Mean conflicts 5-year
+gem <- gem[order(gem$orig, gem$type, gem$year), ]
+gem <- gem %>%
+  group_by(orig, type) %>%
+  mutate(mconfl5 = rollmean(conflicts, k = 5, align = "right", fill = NA))
+# Mean deaths 5-year
+gem <- gem[order(gem$orig, gem$type, gem$year), ]
+gem <- gem %>%
+  group_by(orig, type) %>%
+  mutate(mdeaths5 = rollmean(deaths, k = 5, align = "right", fill = NA))
+# Variation value 5-years
+gem <- gem[order(gem$orig, gem$type, gem$year), ]
+gem <- gem %>%
+  group_by(orig, type) %>%
+  mutate(vvalue5 = value - lag(value, n = 4))
+# Variation conflict 5-years
+gem <- gem[order(gem$orig, gem$type, gem$year), ]
+gem <- gem %>%
+  group_by(orig, type) %>%
+  mutate(vconfl5 = conflicts - lag(conflicts, n = 4))
+
+
+
+
+
+
+
+
+
+
 
 
 
