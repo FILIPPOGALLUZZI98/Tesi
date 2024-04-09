629aebf3c76bbfb411061791feb89e73031afff6
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-09 15:07:07 +0200

    Update 102.Prep_JDatasets.R

--- 102.Prep_JDatasets.R
+++ 102.Prep_JDatasets.R
@@ -6,25 +6,30 @@ suppressPackageStartupMessages({
   library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
   library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign)})
 
-gw_g <- read.csv("^Data/gws.csv")
+gw <- read.csv("^Data/gws.csv")
 events <- read.csv("^Data/events.csv")
 migr <- read.csv("^Data/migr.csv")
 
 ##############################################################################################################################
 ####  GLOBAL JOINT DATASET GW-EVENTS  ########################################################################################
 
-gw_data_g <- gw_g %>%
+gw_data <- gw %>%
   filter(year > 1988)
-events <- events %>%
+events_data <- events %>%
   filter(year<2020)
+
 vettore <- expand.grid(year=1989:2019, type=c("state","Nstate","onesided"))
-gw_events_g <- left_join(gw_data_g, vettore, by=c("year"))
+gw_events <- left_join(gw_data, vettore, by=c("year"))
 
 # Merge the datasets
-gw_events <- left_join(gw_events_g,events,by=c("country","region","year","type","GEOLEVEL1"))
+gw_events <- left_join(gw_events,events_data,by=c("country","region","year","type","GEOLEVEL1"))
 gw_events$deaths[is.na(gw_events$deaths)] = 0  ## Assign a zero to each month/province where no data is observed
 gw_events$conflicts[is.na(gw_events$conflicts)] = 0  ## Assign a zero to each month/province where no data is observed
 gw_events <- gw_events[, c("year","country", "region","type","deaths", "conflicts","value","GEOLEVEL1")]
+
+# Rename the variable
+gw_events <- gw_events %>%
+  rename(orig=GEOLEVEL1)
 
 # Save data
 write.csv(gw_events, paste0("^Data/", "gws_events", ".csv"), row.names=FALSE)
@@ -33,29 +38,28 @@ write.csv(gw_events, paste0("^Data/", "gws_events", ".csv"), row.names=FALSE)
 ##############################################################################################################################
 ####  GLOBAL JOINT DATASET GW-MIGR  ##########################################################################################
 
-# Select the timespan for GW
-gw_data_g <- gw_g %>%
-  filter(year > 1959 & year<2018)
 # Rename the variable
-gw_data_g <- gw_data_g %>%
+gw_data <- gw %>%
   rename(orig=GEOLEVEL1)
+# Select the timespan for GW
+gw_data <- gw_data %>%
+  filter(year > 1959 & year<2018)
 
-# Check for countries without GEOLEV1
-na_val <- subset(gw_data_g, is.na(orig))$country
-a <- unique(countries_with_na); b <- unique(data_migr$country)
-intersect(a, b)
-# Check if the datasets gw_data_g cointains all the regions of data_migr
-length(intersect(unique(gw_data_g$orig), data_migr$orig))
-length(qunique(data_migr$orig))
+## Check for countries without GEOLEV1
+# na_val <- subset(gw_data, is.na(orig))$country
+# a <- unique(countries_with_na); b <- unique(data_migr$country)
+# intersect(a, b)
+# # Check if the datasets gw_data cointains all the regions of data_migr
+# length(intersect(unique(gw_data$orig), data_migr$orig))
+# length(qunique(data_migr$orig))
+# Remove NA values from gw_data
+# gw_data <- na.omit(gw_data[!is.na(gw_data$orig), ])
 
-# Remove NA values from gw_data_g
-# gw_data_g <- na.omit(gw_data_g[!is.na(gw_data_g$orig), ])
-
-# Convert the values of 'orig' in gw_data_g into integers
-gw_data_g$orig <- as.integer(gw_data_g$orig)
+# Convert the values of 'orig' in gw_data into integers
+gw_data$orig <- as.integer(gw_data$orig)
 
 # Merge the datasets
-gw_migr <- left_join(gw_data_g, data_migr, by=c("year", "orig"))
+gw_migr <- left_join(gw_data, migr, by=c("year", "orig"))
 
 # Remove NA values
 # gw_migr <- na.omit(gw_migr[!is.na(gw_migr$population), ])
@@ -65,10 +69,39 @@ gw_migr <- gw_migr %>%
   rename(country=country.x)
 gw_migr$country.y=NULL
 gw_migr <- gw_migr[,c("year", "country", "region", "worldregion", "value", "population","interval", "flow","flow_annual",
-                      "outflow_rate_annual","year_cat10", "orig", "CNTRY_CODE","BPL_CODE")]
+                      "outflow_rate_annual","year_cat10", "orig")]
 
 # Save data
 write.csv(gw_migr, paste0("^Data/", "gws_migr", ".csv"), row.names=FALSE)
+
+
+
+##############################################################################################################################
+####  GLOBAL JOINT DATASET GW-MIGR-CONFL  ####################################################################################
+
+
+gw_migr_conf <- left_join(gw_events, migr, by=c("year", "orig"))
+gw_migr_conf <- gw_migr_conf %>%
+  rename(country=country.x)
+gw_migr_conf$country.y=NULL
+
+# Save data
+write.csv(gw_migr, paste0("^Data/", "gws_migr_events", ".csv"), row.names=FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
