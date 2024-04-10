8f84dc987657234317afcfd30a6d629677fb2f21
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-10 13:17:32 +0200

    Update Prova.R

--- Prova.R
+++ Prova.R
@@ -11,7 +11,7 @@ suppressPackageStartupMessages({
 gem <- read.csv("^Data/gws_migr_events.csv")
 
 ##############################################################################################################################
-#### ALL CONFLICTS  ####
+#### ALL CONFLICTS OLS-GLM  ####
 events_sum <- subset(gem, type=="state")
 
 ## CONFLICTS OLS-GLM
@@ -32,7 +32,7 @@ table <-etable(glm_v, glm_mv1, glm_mv5, glm_vv1, glm_vv5, tex=TRUE)
 write.table(table, file = "^Tables/glm_conflicts.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 
 
-## DEATHS OLS-GLM
+#### ALL DEATHS OLS-GLM  ####
 ols_v <- fixest::feols(data=events_sum, log(1+all_deaths)~value|region + year)
 ols_mv1 <- fixest::feols(data=events_sum, log(1+all_deaths)~mvalue1|region + year)
 ols_mv5 <- fixest::feols(data=events_sum, log(1+all_deaths)~mvalue5|region + year)
@@ -47,15 +47,93 @@ glm_mv5 <- fixest::feglm(data=events_sum, all_deaths~mvalue5|region + year, fami
 glm_vv1 <- fixest::feglm(data=events_sum, all_deaths~vvalue1|region + year, family=quasipoisson)
 glm_vv5 <- fixest::feglm(data=events_sum, all_deaths~sdvalue5|region + year, family=quasipoisson)
 table <-etable(glm_v, glm_mv1, glm_mv5, glm_vv1, glm_vv5, tex=TRUE)
-write.table(table, file = "^Tables/glm_conflicts.txt", sep = "\t", quote = FALSE, row.names = FALSE)
-
-
-
-
-
+write.table(table, file = "^Tables/glm_deaths.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 
 
 ##############################################################################################################################
+
+data_state <- subset(gem, type=="state")
+glm_v <- fixest::feglm(data=data_state, conflicts~value|region + year, family=quasipoisson)
+glm_mv1 <- fixest::feglm(data=data_state, conflicts~mvalue1|region + year, family=quasipoisson)
+glm_mv5 <- fixest::feglm(data=data_state, conflicts~mvalue5|region + year, family=quasipoisson)
+glm_vv1 <- fixest::feglm(data=data_state, conflicts~vvalue1|region + year, family=quasipoisson)
+glm_vv5 <- fixest::feglm(data=data_state, conflicts~sdvalue5|region + year, family=quasipoisson)
+table <-etable(glm_v, glm_mv1, glm_mv5, glm_vv1, glm_vv5, tex=TRUE)
+write.table(table, file = "^Tables/glm_confl_state.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+
+data_onesided <- subset(gem, type=="onesided")
+glm_v <- fixest::feglm(data=data_onesided, conflicts~value|region + year, family=quasipoisson)
+glm_mv1 <- fixest::feglm(data=data_onesided, conflicts~mvalue1|region + year, family=quasipoisson)
+glm_mv5 <- fixest::feglm(data=data_onesided, conflicts~mvalue5|region + year, family=quasipoisson)
+glm_vv1 <- fixest::feglm(data=data_onesided, conflicts~vvalue1|region + year, family=quasipoisson)
+glm_vv5 <- fixest::feglm(data=data_onesided, conflicts~sdvalue5|region + year, family=quasipoisson)
+table <-etable(glm_v, glm_mv1, glm_mv5, glm_vv1, glm_vv5, tex=TRUE)
+write.table(table, file = "^Tables/glm_confl_onesided.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+
+data_Nstate <- subset(gem, type=="Nstate")
+glm_v <- fixest::feglm(data=data_Nstate, conflicts~value|region + year, family=quasipoisson)
+glm_mv1 <- fixest::feglm(data=data_Nstate, conflicts~mvalue1|region + year, family=quasipoisson)
+glm_mv5 <- fixest::feglm(data=data_Nstate, conflicts~mvalue5|region + year, family=quasipoisson)
+glm_vv1 <- fixest::feglm(data=data_Nstate, conflicts~vvalue1|region + year, family=quasipoisson)
+glm_vv5 <- fixest::feglm(data=data_Nstate, conflicts~sdvalue5|region + year, family=quasipoisson)
+table <-etable(glm_v, glm_mv1, glm_mv5, glm_vv1, glm_vv5, tex=TRUE)
+write.table(table, file = "^Tables/glm_confl_Nstate.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+
+
+##############################################################################################################################
+
+world <- c("Africa", "Europe", "Oceania", "Asia")
+
+for (continent in world){
+  get_continent <- function(countries) {
+    countrycode(countries, "country.name", "continent")}
+  data_state <- subset(gem, country==i)
+  data_continent <- gem %>%
+    filter(get_continent(country)==continent)
+  glm_v <- fixest::feglm(data=data_continent, conflicts~value|region + year, family=quasipoisson)
+  glm_mv1 <- fixest::feglm(data=data_continent, conflicts~mvalue1|region + year, family=quasipoisson)
+  glm_mv5 <- fixest::feglm(data=data_continent, conflicts~mvalue5|region + year, family=quasipoisson)
+  glm_vv1 <- fixest::feglm(data=data_continent, conflicts~vvalue1|region + year, family=quasipoisson)
+  glm_vv5 <- fixest::feglm(data=data_continent, conflicts~sdvalue5|region + year, family=quasipoisson)
+  table <-etable(glm_v, glm_mv1, glm_mv5, glm_vv1, glm_vv5, tex=TRUE)
+  write.table(table, file = paste0("^Tables/glm_confl_",continent, ".txt"), sep = "\t", quote = FALSE, row.names = FALSE)
+}
+
+North_America <- c("United States", "Canada", "Mexico")
+N_A <- gem[gem$country %in% North_America, ]
+glm_v <- fixest::feglm(data=N_A, conflicts~value|region + year, family=quasipoisson)
+glm_mv1 <- fixest::feglm(data=N_A, conflicts~mvalue1|region + year, family=quasipoisson)
+glm_mv5 <- fixest::feglm(data=N_A, conflicts~mvalue5|region + year, family=quasipoisson)
+glm_vv1 <- fixest::feglm(data=N_A, conflicts~vvalue1|region + year, family=quasipoisson)
+glm_vv5 <- fixest::feglm(data=N_A, conflicts~sdvalue5|region + year, family=quasipoisson)
+table <-etable(glm_v, glm_mv1, glm_mv5, glm_vv1, glm_vv5, tex=TRUE)
+write.table(table, file = "^Tables/glm_confl_North_America.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+
+
+South_America <- c("Belize", "Costa Rica", "El Salvador", "Guatemala", "Honduras", "Nicaragua", "Panama", "Argentina", "Bolivia", "Brasile", "Cile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Perù", "Suriname", "Uruguay", "Venezuela", "Aruba", "Bahamas", "Barbados", "Cuba", "Dominica", "Giamaica", "Haiti", "Trinidad and Tobago", "Sint Maarten", "Saint Vincent and the Grenadines", "Saint Lucia", "Saint Kitts and Nevis", "Puerto Rico", "Dominican Republic", "Grenada", "Martinique", "Saint Martin", "Virgin Islands", "Turks and Caicos Islands", "Cayman Islands", "British Virgin Islands", "Guadeloupe", "Antigua and Barbuda", "Bonaire", "Curacao", "Saint Barthelemy", "Saba", "Saint Eustatius", "Saint Pierre and Miquelon", "Falkland Islands", "French Guiana", "South Georgia and the South Sandwich Islands", "British West Indies")
+S_A <- gem[gem$country %in% South_America, ]
+glm_v <- fixest::feglm(data=S_A, conflicts~value|region + year, family=quasipoisson)
+glm_mv1 <- fixest::feglm(data=S_A, conflicts~mvalue1|region + year, family=quasipoisson)
+glm_mv5 <- fixest::feglm(data=S_A, conflicts~mvalue5|region + year, family=quasipoisson)
+glm_vv1 <- fixest::feglm(data=S_A, conflicts~vvalue1|region + year, family=quasipoisson)
+glm_vv5 <- fixest::feglm(data=S_A, conflicts~sdvalue5|region + year, family=quasipoisson)
+table <-etable(glm_v, glm_mv1, glm_mv5, glm_vv1, glm_vv5, tex=TRUE)
+write.table(table, file = "^Tables/glm_confl_South_America.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
