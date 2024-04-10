2e8d19472123c129e49e5af1ac26dff0c6bc867a
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-10 11:56:16 +0200

    Update Prova.R

--- Prova.R
+++ Prova.R
@@ -11,50 +11,43 @@ suppressPackageStartupMessages({
 gem <- read.csv("^Data/gws_migr_events.csv")
 
 ##############################################################################################################################
-
+#### ALL CONFLICTS  ####
 events_sum <- subset(gem, type=="state")
 
-####  VALUE - SUM OF EVENTS  #####
-ols_c <- fixest::feols(data=events_sum, log(1+all_confl)~value|region + year)
-glm_c <- fixest::feglm(data=events_sum, all_confl~value|region + year, family=quasipoisson)
-ols_d <- fixest::feols(data=events_sum, log(1+all_deaths)~value|region + year)
-glm_d <- fixest::feglm(data=events_sum, all_deaths~value|region + year, family=quasipoisson)
-table <-etable(ols_c, glm_c, ols_d, glm_d, tex=TRUE)
-write.table(table, file = "^Tables/value_sum_events.txt", sep = "\t", quote = FALSE)
+## CONFLICTS OLS-GLM
+ols_v <- fixest::feols(data=events_sum, log(1+all_confl)~value|region + year)
+ols_mv1 <- fixest::feols(data=events_sum, log(1+all_confl)~mvalue1|region + year)
+ols_mv5 <- fixest::feols(data=events_sum, log(1+all_confl)~mvalue5|region + year)
+ols_vv1 <- fixest::feols(data=events_sum, log(1+all_confl)~vvalue1|region + year)
+ols_vv5 <- fixest::feols(data=events_sum, log(1+all_confl)~sdvalue5|region + year)
+table <-etable(ols_v, ols_mv1, ols_mv5, ols_vv1, ols_vv5, tex=TRUE)
+write.table(table, file = "^Tables/ols_conflicts.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 
-####  MEAN VALUE 1 - SUM OF EVENTS  #####
-ols_c <- fixest::feols(data=events_sum, log(1+all_confl)~mvalue1|region + year)
-glm_c <- fixest::feglm(data=events_sum, all_confl~mvalue1|region + year, family=quasipoisson)
-ols_d <- fixest::feols(data=events_sum, log(1+all_deaths)~mvalue1|region + year)
-glm_d <- fixest::feglm(data=events_sum, all_deaths~mvalue1|region + year, family=quasipoisson)
-table <-etable(ols_c, glm_c, ols_d, glm_d, tex=TRUE)
-write.table(table, file = "^Tables/mvalue1_sum_events.txt", sep = "\t", quote = FALSE)
-
-####  MEAN VALUE 5 - SUM OF EVENTS  #####
-ols_c <- fixest::feols(data=events_sum, log(1+all_confl)~mvalue5|region + year)
-glm_c <- fixest::feglm(data=events_sum, all_confl~mvalue5|region + year, family=quasipoisson)
-ols_d <- fixest::feols(data=events_sum, log(1+all_deaths)~mvalue5|region + year)
-glm_d <- fixest::feglm(data=events_sum, all_deaths~mvalue5|region + year, family=quasipoisson)
-table <-etable(ols_c, glm_c, ols_d, glm_d, tex=TRUE)
-write.table(table, file = "^Tables/mvalue5_sum_events.txt", sep = "\t", quote = FALSE)
-
-####  VARIATION VALUE 1 - SUM OF EVENTS  #####
-ols_c <- fixest::feols(data=events_sum, log(1+all_confl)~vvalue1|region + year)
-glm_c <- fixest::feglm(data=events_sum, all_confl~vvalue1|region + year, family=quasipoisson)
-ols_d <- fixest::feols(data=events_sum, log(1+all_deaths)~vvalue1|region + year)
-glm_d <- fixest::feglm(data=events_sum, all_deaths~vvalue1|region + year, family=quasipoisson)
-table <-etable(ols_c, glm_c, ols_d, glm_d, tex=TRUE)
-write.table(table, file = "^Tables/vvalue1_sum_events.txt", sep = "\t", quote = FALSE)
-
-####  VARIATION VALUE 5 - SUM OF EVENTS  #####
-ols_c <- fixest::feols(data=events_sum, log(1+all_confl)~sdvalue5|region + year)
-glm_c <- fixest::feglm(data=events_sum, all_confl~sdvalue5|region + year, family=quasipoisson)
-ols_d <- fixest::feols(data=events_sum, log(1+all_deaths)~sdvalue5|region + year)
-glm_d <- fixest::feglm(data=events_sum, all_deaths~sdvalue5|region + year, family=quasipoisson)
-table <-etable(ols_c, glm_c, ols_d, glm_d, tex=TRUE)
-write.table(table, file = "^Tables/vvalue5_sum_events.txt", sep = "\t", quote = FALSE)
+glm_v <- fixest::feglm(data=events_sum, all_confl~value|region + year, family=quasipoisson)
+glm_mv1 <- fixest::feglm(data=events_sum, all_confl~mvalue1|region + year, family=quasipoisson)
+glm_mv5 <- fixest::feglm(data=events_sum, all_confl~mvalue5|region + year, family=quasipoisson)
+glm_vv1 <- fixest::feglm(data=events_sum, all_confl~vvalue1|region + year, family=quasipoisson)
+glm_vv5 <- fixest::feglm(data=events_sum, all_confl~sdvalue5|region + year, family=quasipoisson)
+table <-etable(glm_v, glm_mv1, glm_mv5, glm_vv1, glm_vv5, tex=TRUE)
+write.table(table, file = "^Tables/glm_conflicts.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 
 
+## DEATHS OLS-GLM
+ols_v <- fixest::feols(data=events_sum, log(1+all_deaths)~value|region + year)
+ols_mv1 <- fixest::feols(data=events_sum, log(1+all_deaths)~mvalue1|region + year)
+ols_mv5 <- fixest::feols(data=events_sum, log(1+all_deaths)~mvalue5|region + year)
+ols_vv1 <- fixest::feols(data=events_sum, log(1+all_deaths)~vvalue1|region + year)
+ols_vv5 <- fixest::feols(data=events_sum, log(1+all_deaths)~sdvalue5|region + year)
+table <-etable(ols_v, ols_mv1, ols_mv5, ols_vv1, ols_vv5, tex=TRUE)
+write.table(table, file = "^Tables/ols_deaths.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+
+glm_v <- fixest::feglm(data=events_sum, all_deaths~value|region + year, family=quasipoisson)
+glm_mv1 <- fixest::feglm(data=events_sum, all_deaths~mvalue1|region + year, family=quasipoisson)
+glm_mv5 <- fixest::feglm(data=events_sum, all_deaths~mvalue5|region + year, family=quasipoisson)
+glm_vv1 <- fixest::feglm(data=events_sum, all_deaths~vvalue1|region + year, family=quasipoisson)
+glm_vv5 <- fixest::feglm(data=events_sum, all_deaths~sdvalue5|region + year, family=quasipoisson)
+table <-etable(glm_v, glm_mv1, glm_mv5, glm_vv1, glm_vv5, tex=TRUE)
+write.table(table, file = "^Tables/glm_conflicts.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 
 
 
