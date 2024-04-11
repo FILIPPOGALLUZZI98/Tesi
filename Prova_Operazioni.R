47d1f358b2bbc466ca7ce6a16a7e124e78925b5b
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-11 13:12:52 +0200

    Update Prova_Operazioni.R

--- Prova_Operazioni.R
+++ Prova_Operazioni.R
@@ -3,7 +3,6 @@ suppressPackageStartupMessages({
   library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign); library(countrycode);
   library(fixest); library(broom);library(knitr)} )
 
-ge <- read.csv("^Data/gws_events.csv")
 gem <- read.csv("^Data/gws_migr_events_temp.csv")
 
 
@@ -25,6 +24,13 @@ gem <- gem %>%
   group_by(country, region, type) %>%
   mutate(vvalue1 = value - lag(value))
 
+# Growth rate value 1-year
+gem <- gem %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(growth_value1=((value-lag(value))/lag(value))*100)
+
+
 # Mean value 5-years
 gem <- gem %>%
   arrange(year, country, region, type) %>%
@@ -37,12 +43,6 @@ gem <- gem %>%
   group_by(country, region, type) %>%
   mutate(sdvalue5 = rollapply(value, width = 5, FUN = sd, align = "right", fill = NA))
 
-# Growth rate value 1-year
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(growth_value1=((value-lag(value))/lag(value))*100)
-
 # Growth rate value 5-year
 gem <- gem %>%
   arrange(year, country, region, type) %>%
@@ -50,64 +50,7 @@ gem <- gem %>%
   mutate(growth_value5=((value-lag(value, n=5))/lag(value, n=5))*100)
 
 
-# Create a new variable with the sum of the conflicts for the same year, country, region for the three types
-gem <- gem %>% 
-  group_by(year, country, region) %>% 
-  mutate(all_confl = sum(conflicts))
-gem <- gem %>% 
-  group_by(year, country, region) %>% 
-  mutate(all_deaths = sum(deaths))
-# Mean conflicts 1-year
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(mconfl1 = (lag(conflicts) + conflicts) / 2)
-# Mean deaths 1-year
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(mdeaths1 = (lag(deaths) + deaths) / 2)
-# Growth rate conflicts 1-year
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(growth_confl1=((conflicts-lag(conflicts))/lag(conflicts))*100)
-# Growth rate deaths 1-year
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(growth_deaths1=((deaths-lag(deaths))/lag(deaths))*100)
-# Mean conflicts 5-years
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(mconflict5 = rollmean(conflicts, k = 5, align = "right", fill = NA))
-# Mean deaths 5-years
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(mdeaths5 = rollmean(deaths, k = 5, align = "right", fill = NA))
-# Growth rate conflicts 5-year
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(growth_confl5=((all_confl-lag(all_confl, n=5))/lag(all_confl, n=5))*100)
-# Growth rate deaths 5-year
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate((growth_deaths5=((all_deaths-lag(all_deaths, n=5))/lag(all_deaths, n=5))*100))
-# Standard deviation conflicts 5-year
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(sconflicts5 = rollapply(conflicts, width = 5, FUN = sd, align = "right", fill = NA))
-# Standard deviation deaths 5-year
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(sddeaths5 = rollapply(deaths, width = 5, FUN = sd, align = "right", fill = NA))
-
+# Anomalies 
 medie <- gem %>%
   select(year, country, region, type, value)
 medie <- medie %>%
@@ -119,7 +62,6 @@ medie$year <- NULL; medie$type <- NULL; medie$value <- NULL
 medie <- medie %>%
   distinct(country, region, .keep_all = TRUE)
 gem <- left_join(gem,medie,by=c("country","region"))
-
 std_t <- gem %>%
   select(year, country, region, type, value)
 std_t <- std_t %>%
@@ -131,11 +73,57 @@ std_t$year <- NULL; std_t$type <- NULL; std_t$value <- NULL
 std_t <- std_t %>%
   distinct(country, region, .keep_all = TRUE)
 gem <- left_join(gem,std_t,by=c("country","region"))
-
 gem <- gem %>%
   arrange(year, country, region, type) %>%
   group_by(country, region, type) %>%
   mutate(anomaly_it = (value-mean_region)/std)
+
+
+
+# Create a new variable with the sum of the conflicts for the same year, country, region for the three types
+gem <- gem %>% 
+  group_by(year, country, region) %>% 
+  mutate(all_confl = sum(conflicts))
+gem <- gem %>% 
+  group_by(year, country, region) %>% 
+  mutate(all_deaths = sum(deaths))
+
+# Mean conflicts 1-year
+gem <- gem %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(mconfl1 = (lag(conflicts) + conflicts) / 2)
+
+# Mean deaths 1-year
+gem <- gem %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(mdeaths1 = (lag(deaths) + deaths) / 2)
+
+# Growth rate conflicts 1-year
+gem <- gem %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(growth_confl1=((conflicts-lag(conflicts))/lag(conflicts))*100)
+
+# Mean conflicts 5-years
+gem <- gem %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(mconflict5 = rollmean(conflicts, k = 5, align = "right", fill = NA))
+
+# Growth rate conflicts 5-year
+gem <- gem %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(growth_confl5=((all_confl-lag(all_confl, n=5))/lag(all_confl, n=5))*100)
+
+# Standard deviation conflicts 5-year
+gem <- gem %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(sconflicts5 = rollapply(conflicts, width = 5, FUN = sd, align = "right", fill = NA))
+
 
 # Save data
 write.csv(gem, paste0("^Data/", "gws_migr_events", ".csv"), row.names=FALSE)
