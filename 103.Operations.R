2f333022fa80e9e554e9a1a9d9ccd74663a2c7fd
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-12 10:27:48 +0200

    Update 103.Operations.R

--- 103.Operations.R
+++ 103.Operations.R
@@ -3,12 +3,82 @@ suppressPackageStartupMessages({
   library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign); library(countrycode);
   library(fixest); library(broom);library(knitr)} )
 
-ge <- read.csv("^Data/gws_events.csv")
 gem <- read.csv("^Data/gws_migr_events_temp.csv")
 
 
 #################################################################################################
 #################################################################################################
+
+# Rescale GW data (dividing by 1,000)
+gem$value <- gem$value/1000
+
+# Mean value 1-year
+gem <- gem %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(mvalue1 = (lag(value) + value)/2)
+
+# Variation value 1-year
+gem <- gem %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(vvalue1 = value - lag(value))
+
+# Growth rate value 1-year
+gem <- gem %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(growth_value1=((value-lag(value))/lag(value))*100)
+
+
+# Mean value 5-years
+gem <- gem %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(mvalue5 = rollmean(value, k = 5, align = "right", fill = NA))
+
+# Standard Deviation value 5-year
+gem <- gem %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(sdvalue5 = rollapply(value, width = 5, FUN = sd, align = "right", fill = NA))
+
+# Growth rate value 5-year
+gem <- gem %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(growth_value5=((value-lag(value, n=5))/lag(value, n=5))*100)
+
+
+# Anomalies 
+medie <- gem %>%
+  select(year, country, region, type, value)
+medie <- medie %>%
+  filter(year >= 1980 & year <= 2010) %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(mean_region = mean(value))
+medie$year <- NULL; medie$type <- NULL; medie$value <- NULL
+medie <- medie %>%
+  distinct(country, region, .keep_all = TRUE)
+gem <- left_join(gem,medie,by=c("country","region"))
+std_t <- gem %>%
+  select(year, country, region, type, value)
+std_t <- std_t %>%
+  filter(year >= 1980 & year <= 2010) %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(std = sd(value))
+std_t$year <- NULL; std_t$type <- NULL; std_t$value <- NULL
+std_t <- std_t %>%
+  distinct(country, region, .keep_all = TRUE)
+gem <- left_join(gem,std_t,by=c("country","region"))
+gem <- gem %>%
+  arrange(year, country, region, type) %>%
+  group_by(country, region, type) %>%
+  mutate(anomaly_it = (value-mean_region)/std)
+
+
 
 # Create a new variable with the sum of the conflicts for the same year, country, region for the three types
 gem <- gem %>% 
@@ -18,72 +88,62 @@ gem <- gem %>%
   group_by(year, country, region) %>% 
   mutate(all_deaths = sum(deaths))
 
-
-# Mean value 1-year
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(mvalue1 = (lag(value) + value) / 2)
 # Mean conflicts 1-year
 gem <- gem %>%
   arrange(year, country, region, type) %>%
   group_by(country, region, type) %>%
   mutate(mconfl1 = (lag(conflicts) + conflicts) / 2)
+
 # Mean deaths 1-year
 gem <- gem %>%
   arrange(year, country, region, type) %>%
   group_by(country, region, type) %>%
   mutate(mdeaths1 = (lag(deaths) + deaths) / 2)
-# Variation value 1-year
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(vvalue1 = value - lag(value))
-# Variation conflicts 1-year
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(vconfl1 = conflicts - lag(conflicts))
-# Vatiation deaths 1-year
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(vdeaths1 = deaths - lag(deaths))
 
-# Mean value 5-years
+# Growth rate conflicts 1-year
 gem <- gem %>%
   arrange(year, country, region, type) %>%
   group_by(country, region, type) %>%
-  mutate(mvalue5 = rollmean(value, k = 5, align = "right", fill = NA))
+  mutate(growth_confl1=((conflicts-lag(conflicts))/lag(conflicts))*100)
+
 # Mean conflicts 5-years
 gem <- gem %>%
   arrange(year, country, region, type) %>%
   group_by(country, region, type) %>%
   mutate(mconflict5 = rollmean(conflicts, k = 5, align = "right", fill = NA))
-# Mean deaths 5-years
+
+# Growth rate conflicts 5-year
 gem <- gem %>%
   arrange(year, country, region, type) %>%
   group_by(country, region, type) %>%
-  mutate(mdeaths5 = rollmean(deaths, k = 5, align = "right", fill = NA))
-# Variation value 5-year
+  mutate(growth_confl5=((all_confl-lag(all_confl, n=5))/lag(all_confl, n=5))*100)
+
+# Standard deviation conflicts 5-year
 gem <- gem %>%
   arrange(year, country, region, type) %>%
   group_by(country, region, type) %>%
-  mutate(sdvalue5 = rollapply(value, width = 5, FUN = sd, align = "right", fill = NA))
-# Variation conflicts 5-year
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(sdvalue5 = rollapply(conflicts, width = 5, FUN = sd, align = "right", fill = NA))
-# Vatiation deaths 5-year
-gem <- gem %>%
-  arrange(year, country, region, type) %>%
-  group_by(country, region, type) %>%
-  mutate(sdvalue5 = rollapply(deaths, width = 5, FUN = sd, align = "right", fill = NA))
+  mutate(sconflicts5 = rollapply(conflicts, width = 5, FUN = sd, align = "right", fill = NA))
 
 
 # Save data
 write.csv(gem, paste0("^Data/", "gws_migr_events", ".csv"), row.names=FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
