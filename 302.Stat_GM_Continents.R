fd666b782c484a80483500a7804e7918611ee1ca
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-15 11:53:23 +0200

    Update 302.Stat_GM_Continents.R

--- 302.Stat_GM_Continents.R
+++ 302.Stat_GM_Continents.R
@@ -1,1 +1,18 @@
+suppressPackageStartupMessages({
+  library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
+  library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign); library(countrycode);
+  library(fixest); library(broom);library(knitr); library(stargazer)} )
+
+gm <- read.csv("^Data/gws_migr.csv")
+
+setFixest_dict(c(migrants="number of migrants/population in the interval", value="groundwater storage [g/m^3]",
+                 gws_avg1="groundwater storage average 1-y", gws_avg5="groundwater storage average 5-y", gws_avg10="groundwater storage average 10-y",
+                 gws_growth1="groundwater growth rate 1-y", gws_growth5="groundwater growth rate 5-y", gws_growth10="groundwater growth rate 10-y",
+                 gws_std1="groundwater standard deviation 1-y", gws_std5="groundwater standard deviation 5-y", gws_std10="groundwater standard deviation 10-y",
+                 gws_anomalies="groundwater anomalies (1980-2010)"))
+
+
+
+
+
 
