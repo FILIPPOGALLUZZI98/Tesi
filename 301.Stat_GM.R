ee184d12d8520a9a146c06fc5d911cde6652dbb3
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-15 11:53:03 +0200

    Update 301.Stat_GM.R

--- 301.Stat_GM.R
+++ 301.Stat_GM.R
@@ -5,23 +5,22 @@ suppressPackageStartupMessages({
 
 gm <- read.csv("^Data/gws_migr.csv")
 
-setFixest_dict(c(conflicts="number of conflicts per type", value="groundwater storage [g/m^3]",
+setFixest_dict(c(migrants="number of migrants/population in the interval", value="groundwater storage [g/m^3]",
                  gws_avg1="groundwater storage average 1-y", gws_avg5="groundwater storage average 5-y", gws_avg10="groundwater storage average 10-y",
                  gws_growth1="groundwater growth rate 1-y", gws_growth5="groundwater growth rate 5-y", gws_growth10="groundwater growth rate 10-y",
                  gws_std1="groundwater standard deviation 1-y", gws_std5="groundwater standard deviation 5-y", gws_std10="groundwater standard deviation 10-y",
-                 gws_anomalies="groundwater anomalies (1980-2010)", count="total number of conflict per year"))
+                 gws_anomalies="groundwater anomalies (1980-2010)"))
 
 
-events_sum <- subset(ge, type=="state")
+data_1 <- subset(gm, interval==1)
+model <- fixest::feols(data=data_1, log(migrants)~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year)
+table<- etable(model, tex=TRUE)
+write.table(table, file = "^Tables/glm_migration_1.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 
-
-
-
-
-
-
-
-ols_v <- fixest::feols(data=events_sum, log(1+all_confl)~value|region + year)
+data_5 <- subset(gm, interval==5)
+model <- fixest::feols(data=data_1, log(migrants)~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year)
+table<- etable(model, tex=TRUE)
+write.table(table, file = "^Tables/glm_migration_5.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 
 
 
