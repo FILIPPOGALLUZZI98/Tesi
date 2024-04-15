c31c7779b144aa00f60e8f9fb8a3726a9027d411
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-15 09:43:03 +0200

    Update 101.Prep_SDatasets.R

--- 101.Prep_SDatasets.R
+++ 101.Prep_SDatasets.R
@@ -22,18 +22,17 @@ r <- raster::brick(paste0("^Data/",rast,".nc"))
 gw_g <- exactextractr::exact_extract(r, shp, fun="mean")
 
 # Add columns for regions and countries
-gw_g$region <- shp$region ; gw_g$country <- shp$country; gw_g$GEOLEVEL1<-shp$GEOLEVEL1
+gw_g$region <- shp$region ; gw_g$country <- shp$country; gw_g$orig<-shp$GEOLEVEL1
 
 # Reshape the dataset into a long form
-gw_g <- reshape2::melt(gw_g, id.vars=c("country", "region", "GEOLEVEL1"))
+gw_g <- reshape2::melt(gw_g, id.vars=c("country", "region", "orig"))
 
 # Rename the years
 gw_g$variable <- gsub("mean.X", "", gw_g$variable)  # Rimuovi "mean.X"
 gw_g$year <- as.integer(gsub("\\D", "", gw_g$variable)) + 1900 
 gw_g$variable=NULL
-gw_g <- gw_g[, c("year","country", "region", "value","GEOLEVEL1")]
-gw_g <- gw_g %>%
-  rename(orig = GEOLEVEL1)
+gw_g <- gw_g[, c("year","country", "region", "value","orig")]
+
 
 
 # Save Data
