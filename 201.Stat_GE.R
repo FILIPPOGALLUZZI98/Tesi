d4973a37b104086073965266166c2395739397d6
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-15 11:21:01 +0200

    Update 201.Stat_GE.R

--- 201.Stat_GE.R
+++ 201.Stat_GE.R
@@ -14,13 +14,9 @@ setFixest_dict(c(conflicts="number of conflicts per type", value="groundwater st
 
 events_sum <- subset(ge, type=="state")
 
-model1 <- fixest::feglm(data=events_sum, count~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_anomalies)|region + year, family=quasipoisson)
-model2 <- fixest::feglm(data=events_sum, count~sw(gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
-table1<- etable(model1, tex=TRUE)
-table2<- etable(model2, tex=TRUE)
-
-write.table(table1, file = "^Tables/glm_conflicts_1.txt", sep = "\t", quote = FALSE, row.names = FALSE)
-write.table(table2, file = "^Tables/glm_conflicts_2.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+model <- fixest::feglm(data=events_sum, count~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
+table<- etable(model, tex=TRUE)
+write.table(table, file = "^Tables/glm_conflicts.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 
 
 
