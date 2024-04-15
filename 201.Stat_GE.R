e339d002bb2849aacd0b7fa59462caca4a0f8d17
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-15 13:59:20 +0200

    Update 201.Stat_GE.R

--- 201.Stat_GE.R
+++ 201.Stat_GE.R
@@ -9,7 +9,7 @@ setFixest_dict(c(conflicts="number of conflicts per type", value="gws [g/m^3]",
                  gws_avg1="gws 1-y", gws_avg5="gws 5-y", gws_avg10="gws 10-y",
                  gws_growth1="gws growth rate 1-y", gws_growth5="gws growth rate 5-y", gws_growth10="gws growth rate 10-y",
                  gws_std1="gws st dev 1-y", gws_std5="gws st dev 5-y", gws_std10="gws st dev 10-y",
-                 gws_anomalies="gws anomalies (1980-2010)", count="total number of conflict per year"))
+                 gws_anomalies="gws anomalies (1980-2010)", count="total conflict"))
   
 
 events_sum <- subset(ge, type=="state")
