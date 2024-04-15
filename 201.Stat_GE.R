a5089b08f926ac66e9b19e4bc3eb0e37a4a74bb1
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-15 14:45:52 +0200

    Update 201.Stat_GE.R

--- 201.Stat_GE.R
+++ 201.Stat_GE.R
@@ -16,7 +16,7 @@ events_sum <- subset(ge, type=="state")
 
 model <- fixest::feglm(data=events_sum, count~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
 table <- xtable(etable(model), tex=TRUE)
-print(table, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts.tex", include.rownames = FALSE)
+print(table, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts.txt", include.rownames = FALSE)
 
 
 
