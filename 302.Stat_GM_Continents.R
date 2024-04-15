e082a21476791eeb283c84b5cf1748fcb7f9200a
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-15 12:00:11 +0200

    Update 302.Stat_GM_Continents.R

--- 302.Stat_GM_Continents.R
+++ 302.Stat_GM_Continents.R
@@ -12,7 +12,58 @@ setFixest_dict(c(migrants="number of migrants/population in the interval", value
                  gws_anomalies="groundwater anomalies (1980-2010)"))
 
 
+data_1 <- subset(gm, interval==1)
 
+continent <- "Africa"
+get_continent <- function(countries) {
+  countrycode(countries, "country.name", "continent")}
+data_continent <- data_1 %>%
+  filter(get_continent(country) == continent)
+Africa <- fixest::feols(data=data_1, log(migrants)~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year)
+table_africa <- etable(Africa, tex=TRUE)
+write.table(table_africa, file = "^Tables/glm_migration_africa.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 
+continent <- "Asia"
+get_continent <- function(countries) {
+  countrycode(countries, "country.name", "continent")}
+data_continent <- data_1 %>%
+  filter(get_continent(country) == continent)
+Asia <- fixest::feols(data=data_1, log(migrants)~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year)
+table_asia <- etable(Asia, tex=TRUE)
+write.table(table_asia, file = "^Tables/glm_migration_asia.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 
+continent <- "Oceania"
+get_continent <- function(countries) {
+  countrycode(countries, "country.name", "continent")}
+data_continent <- data_1 %>%
+  filter(get_continent(country) == continent)
+Oceania <- fixest::feols(data=data_1, log(migrants)~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year)
+table_oceania <- etable(Oceania, tex=TRUE)
+write.table(table_oceania, file = "^Tables/glm_migration_oceania.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 
+continent <- "Europe"
+get_continent <- function(countries) {
+  countrycode(countries, "country.name", "continent")}
+data_continent <- data_1 %>%
+  filter(get_continent(country) == continent)
+Europe <- fixest::feols(data=data_1, log(migrants)~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year)
+table_europe <- etable(Europe, tex=TRUE)
+write.table(table_europe, file = "^Tables/glm_migration_europe.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+
+North_America <- c("United States", "Canada", "Mexico")
+N_A <- data_1[data_1$country %in% North_America, ]
+Namerica <- fixest::feols(data=data_1, log(migrants)~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year)
+table_Namerica <- etable(Namerica, tex=TRUE)
+write.table(table_Namerica, file = "^Tables/glm_migration_Namerica.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+
+South_America <- c("Argentina", "Bolivia", "Brasile", "Cile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Perù", "Suriname", "Uruguay", "Venezuela", "Aruba", "Falkland Islands", "French Guiana", "South Georgia and the South Sandwich Islands")
+S_A <- data_1[data_1$country %in% South_America, ]
+Samerica <- fixest::feols(data=data_1, log(migrants)~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year)
+table_Samerica <- etable(Samerica, tex=TRUE)
+write.table(table_Samerica, file = "^Tables/glm_migration_Samerica.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+
+Central_America <- c("Belize", "Costa Rica", "El Salvador", "Guatemala", "Honduras", "Nicaragua", "Panama", "Bahamas", "Barbados", "Cuba", "Dominica", "Giamaica", "Haiti", "Trinidad and Tobago", "Sint Maarten", "Saint Vincent and the Grenadines", "Saint Lucia", "Saint Kitts and Nevis", "Puerto Rico", "Dominican Republic", "Grenada", "Martinique", "Saint Martin", "Virgin Islands", "Turks and Caicos Islands", "Cayman Islands", "British Virgin Islands", "Guadeloupe", "Antigua and Barbuda", "Bonaire", "Curacao", "Saint Barthelemy", "Saba", "Saint Eustatius", "Saint Pierre and Miquelon", "British West Indies")
+C_A <- data_1[data_1$country %in% Central_America, ]
+Camerica <- fixest::feols(data=data_1, log(migrants)~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year)
+table_Camerica <- etable(Camerica, tex=TRUE)
+write.table(table_Camerica, file = "^Tables/glm_migration_Camerica.txt", sep = "\t", quote = FALSE, row.names = FALSE)
