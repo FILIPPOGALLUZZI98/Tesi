fe08c66012777d0d16daa0acb532e4357fecd45f
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-15 11:00:59 +0200

    Update 201.Stat_GE.R

--- 201.Stat_GE.R
+++ 201.Stat_GE.R
@@ -9,17 +9,15 @@ setFixest_dict(c(conflicts="number of conflicts per type", value="nroundwater St
                  gws_avg1="groundwater storage average 1-y", gws_avg5="groundwater storage average 5-y", gws_avg10="groundwater storage average 10-y",
                  gws_growth1="groundwater growth rate 1-y", gws_growth5="groundwater growth rate 5-y", gws_growth10="groundwater growth rate 10-y",
                  gws_std1="groundwater standard deviation 1-y", gws_std5="groundwater standard deviation 5-y", gws_std10="groundwater standard deviation 10-y",
-                 gws_anomalies="groundwater anomalies (1980-2010)", count="total number of conflict per year",
-                 confl_avg1="number of conflict average per type 1-y", confl_avg5="number of conflict average per type 5-y", confl_avg10="number of conflict average per type 10-y",
-                 count_avg1="total number of conflict average 1-y", count_avg5="total number of conflict average 5-y", count_avg10="total number of conflict average 10-y"))
+                 gws_anomalies="groundwater anomalies (1980-2010)", count="total number of conflict per year"))
   
 
 events_sum <- subset(ge, type=="state")
 
-gws1 <- fixest::feglm(data=events_sum, count~sw(value,gws_avg1,gws_avg5,gws_avg10)|region + year, family=quasipoisson)
-gws2 <- fixest::feglm(data=events_sum, count~sw(mvalue5,growth_value5,sdvalue5,anomaly_it)|region + year, family=quasipoisson)
-table1<- etable(gws1, tex=TRUE)
-table2<- etable(gws2, tex=TRUE)
+model1 <- fixest::feglm(data=events_sum, count~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
+model2 <- fixest::feglm(data=events_sum, count~sw(gws_anomalies,gws_std1, gws_std5,gws_std10)|region + year, family=quasipoisson)
+table1<- etable(model1, tex=TRUE)
+table2<- etable(model2, tex=TRUE)
 
 write.table(table1, file = "^Tables/glm_conflicts_1.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 write.table(table2, file = "^Tables/glm_conflicts_2.txt", sep = "\t", quote = FALSE, row.names = FALSE)
