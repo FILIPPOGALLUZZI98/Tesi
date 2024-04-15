8444abf2556f22a84a157fab7e52fa165a683cc7
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-15 14:16:17 +0200

    Update 202.Stat.GE_Continents.R

--- 202.Stat.GE_Continents.R
+++ 202.Stat.GE_Continents.R
@@ -19,8 +19,8 @@ get_continent <- function(countries) {
 data_continent <- events_sum %>%
   filter(get_continent(country) == continent)
 Africa <- fixest::feglm(data = data_continent, count ~ sw(value,gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10) | region + year, family = quasipoisson)
-table_africa <- etable(Africa, tex=TRUE)
-write.table(table_africa, file = "^Tables/glm_conflicts_africa.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+table_africa <- xtable(etable(Africa), tex=TRUE)
+print(table_africa, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts_africa.txt", include.rownames = FALSE)
 
 continent <- "Asia"
 get_continent <- function(countries) {
@@ -28,8 +28,8 @@ get_continent <- function(countries) {
 data_continent <- events_sum %>%
   filter(get_continent(country) == continent)
 Asia <- fixest::feglm(data = data_continent, count ~ sw(value,gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10) | region + year, family = quasipoisson)
-table_asia <- etable(Asia, tex=TRUE)
-write.table(table_asia, file = "^Tables/glm_conflicts_asia.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+table_asia <- xtable(etable(Asia), tex=TRUE)
+print(table_asia, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts_asia.txt", include.rownames = FALSE)
 
 continent <- "Oceania"
 get_continent <- function(countries) {
@@ -37,8 +37,8 @@ get_continent <- function(countries) {
 data_continent <- events_sum %>%
   filter(get_continent(country) == continent)
 Oceania <- fixest::feglm(data = data_continent, count ~ sw(value,gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10) | region + year, family = quasipoisson)
-table_oceania <- etable(Oceania, tex=TRUE)
-write.table(table_oceania, file = "^Tables/glm_conflicts_oceania.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+table_oceania <- xtable(etable(Oceania), tex=TRUE)
+print(table_oceania, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts_oceania.txt", include.rownames = FALSE)
 
 continent <- "Europe"
 get_continent <- function(countries) {
@@ -46,27 +46,26 @@ get_continent <- function(countries) {
 data_continent <- events_sum %>%
   filter(get_continent(country) == continent)
 Europe <- fixest::feglm(data = data_continent, count ~ sw(value,gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10) | region + year, family = quasipoisson)
-table_europe <- etable(Europe, tex=TRUE)
-write.table(table_europe, file = "^Tables/glm_conflicts_europe.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+table_europe <- xtable(etable(Europe), tex=TRUE)
+print(table_europe, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts_europe.txt", include.rownames = FALSE)
 
 North_America <- c("United States", "Canada", "Mexico")
 N_A <- events_sum[events_sum$country %in% North_America, ]
 Namerica <- fixest::feglm(data = N_A, count ~ sw(value,gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10) | region + year, family = quasipoisson)
-table_Namerica <- etable(Namerica, tex=TRUE)
-write.table(table_Namerica, file = "^Tables/glm_conflicts_Namerica.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+table_Namerica <- xtable(etable(Namerica), tex=TRUE)
+print(table_Namerica, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts_Namerica.txt", include.rownames = FALSE)
 
 South_America <- c("Argentina", "Bolivia", "Brasile", "Cile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Perù", "Suriname", "Uruguay", "Venezuela", "Aruba", "Falkland Islands", "French Guiana", "South Georgia and the South Sandwich Islands")
 S_A <- events_sum[events_sum$country %in% South_America, ]
 Samerica <- fixest::feglm(data = S_A, count ~ sw(value,gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10) | region + year, family = quasipoisson)
-table_Samerica <- etable(Samerica, tex=TRUE)
-write.table(table_Samerica, file = "^Tables/glm_conflicts_Samerica.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+table_Samerica <- xtable(etable(Samerica), tex=TRUE)
+print(table_Samerica, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts_Samerica.txt", include.rownames = FALSE)
 
 Central_America <- c("Belize", "Costa Rica", "El Salvador", "Guatemala", "Honduras", "Nicaragua", "Panama", "Bahamas", "Barbados", "Cuba", "Dominica", "Giamaica", "Haiti", "Trinidad and Tobago", "Sint Maarten", "Saint Vincent and the Grenadines", "Saint Lucia", "Saint Kitts and Nevis", "Puerto Rico", "Dominican Republic", "Grenada", "Martinique", "Saint Martin", "Virgin Islands", "Turks and Caicos Islands", "Cayman Islands", "British Virgin Islands", "Guadeloupe", "Antigua and Barbuda", "Bonaire", "Curacao", "Saint Barthelemy", "Saba", "Saint Eustatius", "Saint Pierre and Miquelon", "British West Indies")
 C_A <- events_sum[events_sum$country %in% Central_America, ]
 Camerica <- fixest::feglm(data = C_A, count ~ sw(value,gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10) | region + year, family = quasipoisson)
-table_Camerica <- etable(Camerica, tex=TRUE)
-write.table(table_Camerica, file = "^Tables/glm_conflicts_Camerica.txt", sep = "\t", quote = FALSE, row.names = FALSE)
-
+table_Camerica <- xtable(etable(Camerica), tex=TRUE)
+print(table_Camerica, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts_Camerica.txt", include.rownames = FALSE)
 
 
 
