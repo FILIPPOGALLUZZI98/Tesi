9a227f3790d3d19967f7870f5cdbd78016407c95
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-15 13:56:42 +0200

    Update 202.Stat.GE_Continents.R

--- 202.Stat.GE_Continents.R
+++ 202.Stat.GE_Continents.R
@@ -1,7 +1,7 @@
 suppressPackageStartupMessages({
   library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
   library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign); library(countrycode);
-  library(fixest); library(broom);library(knitr); library(stargazer)} )
+  library(fixest); library(broom);library(knitr); library(stargazer); library(xtable)} )
 
 ge <- read.csv("^Data/gws_events.csv")
 
