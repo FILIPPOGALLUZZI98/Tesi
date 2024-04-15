f86e33003e357503b196e1becf160b682a2d885e
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-15 11:06:46 +0200

    Update 201.Stat_GE.R

--- 201.Stat_GE.R
+++ 201.Stat_GE.R
@@ -5,7 +5,7 @@ suppressPackageStartupMessages({
 
 ge <- read.csv("^Data/gws_events.csv")
 
-setFixest_dict(c(conflicts="number of conflicts per type", value="nroundwater Storage [g/m^3]",
+setFixest_dict(c(conflicts="number of conflicts per type", value="groundwater storage [g/m^3]",
                  gws_avg1="groundwater storage average 1-y", gws_avg5="groundwater storage average 5-y", gws_avg10="groundwater storage average 10-y",
                  gws_growth1="groundwater growth rate 1-y", gws_growth5="groundwater growth rate 5-y", gws_growth10="groundwater growth rate 10-y",
                  gws_std1="groundwater standard deviation 1-y", gws_std5="groundwater standard deviation 5-y", gws_std10="groundwater standard deviation 10-y",
@@ -14,8 +14,8 @@ setFixest_dict(c(conflicts="number of conflicts per type", value="nroundwater St
 
 events_sum <- subset(ge, type=="state")
 
-model1 <- fixest::feglm(data=events_sum, count~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
-model2 <- fixest::feglm(data=events_sum, count~sw(gws_anomalies,gws_std1, gws_std5,gws_std10)|region + year, family=quasipoisson)
+model1 <- fixest::feglm(data=events_sum, count~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_anomalies)|region + year, family=quasipoisson)
+model2 <- fixest::feglm(data=events_sum, count~sw(gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
 table1<- etable(model1, tex=TRUE)
 table2<- etable(model2, tex=TRUE)
 
