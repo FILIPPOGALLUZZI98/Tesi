c7c7cee4c2498c2b1a2cf545a3697021e42de06e
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-25 10:21:41 +0200

    Update 301.Stat_GM.R

--- 301.Stat_GM.R
+++ 301.Stat_GM.R
@@ -9,7 +9,7 @@ setFixest_dict(c(migrants="number of migrants/pop in interval", value="gws [g/m^
                  gws_avg1="gws 1-y", gws_avg5="gws 5-y", gws_avg10="gws 10-y",
                  gws_growth1="gws growth rate 1-y", gws_growth5="gws growth rate 5-y", gws_growth10="gws growth rate 10-y",
                  gws_std1="gws st dev 1-y", gws_std5="gws st dev 5-y", gws_std10="gws st dev 10-y",
-                 gws_anomalies="gws anomalies 1y (1980-2010)", gws_anomalies="gws anomalies 5y (1980-2010)",
+                 gws_anomalies="gws anomalies 1y (1980-2010)", gws_anomalies5="gws anomalies 5y (1980-2010)",
                  gws_anomalies10="gws anomalies 10y (1980-2010)"))
 
 
