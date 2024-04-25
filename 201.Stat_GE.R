3eb3b724beb3354ab4cc1b72a3c4112965920cef
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-04-25 09:51:42 +0200

    Update 201.Stat_GE.R

--- 201.Stat_GE.R
+++ 201.Stat_GE.R
@@ -9,12 +9,13 @@ setFixest_dict(c(conflicts="Type conflicts", value="gws [g/m^3]",
                  gws_avg1="gws 1-y", gws_avg5="gws 5-y", gws_avg10="gws 10-y",
                  gws_growth1="gws growth rate 1-y", gws_growth5="gws growth rate 5-y", gws_growth10="gws growth rate 10-y",
                  gws_std1="gws st dev 1-y", gws_std5="gws st dev 5-y", gws_std10="gws st dev 10-y",
-                 gws_anomalies="gws anomalies (1980-2010)", count="total conflict"))
+                 gws_anomalies="gws anomalies 1y (1980-2010)", gws_anomalies5="gws anomalies 5y (1980-2010)",
+                 gws_anomalies10="gws anomalies 10y (1980-2010)",count="total conflict"))
   
 
 events_sum <- subset(ge, type=="state")
 
-model <- fixest::feglm(data=events_sum, count~sw(value,gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
+model <- fixest::feglm(data=events_sum, count~sw(gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_anomalies5, gws_anomalies10, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
 table <- xtable(etable(model), tex=TRUE)
 print(table, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts.tex", include.rownames = FALSE)
 
