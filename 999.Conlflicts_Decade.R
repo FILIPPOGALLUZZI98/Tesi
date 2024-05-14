b6303f47b8b3ba64dbe7c99295a0b3f68ca6d3ec
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-05-14 10:26:14 +0200

    Update 999.Conlflicts_Decade.R

--- 999.Conlflicts_Decade.R
+++ 999.Conlflicts_Decade.R
@@ -1,1 +1,55 @@
+file_path <- "^Data_Raw/Conflict_Data/Global.csv"
+events <- read.csv(file_path)
+
+# Rename the variables
+events <- events %>%
+  rename(type = type_of_violence,
+         number_deaths = best)
+events <- mutate(events,
+                 type = case_when(
+                   type == 1 ~ "state",
+                   type == 2 ~ "Nstate",
+                   type == 3 ~ "onesided"
+                 ))
+
+# Set the coordinate system
+events <- st_as_sf(events, coords = c("longitude", "latitude"), crs = st_crs(shp))
+events <- st_transform(events, st_crs(shp))
+
+# Remove the invalid geometries from shp
+shp <- shp[st_is_valid(shp), ]
+
+
+events1 <- subset(events, year >= 1989 & year <= 1999)
+events2 <- subset(events, year >= 2000 & year <= 2009)
+events3 <- subset(events, yera >= 2019 & yera <= 2019)
+
+plot_final <- ggplot() +
+  geom_sf(data = shp) +
+  geom_sf(data = events1, color = "red")
+print(plot_final)
+
+ggplot() +
+  geom_sf(data = shp) +
+  geom_sf(data = events2, color = "red")
+
+ggplot() +
+  geom_sf(data = shp) +
+  geom_sf(data = events3, color = "red")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
