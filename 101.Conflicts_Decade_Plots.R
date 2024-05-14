89a4a2936a10e2c591cd9ea080a8f02fbde07de7
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-05-14 12:26:04 +0200

    Update 101.Conflicts_Decade_Plots.R

--- 101.Conflicts_Decade_Plots.R
+++ 101.Conflicts_Decade_Plots.R
@@ -1,24 +1,4 @@
-file_path <- "^Data_Raw/Conflict_Data/Global.csv"
-events <- read.csv(file_path)
-
-# Rename the variables
-events <- events %>%
-  rename(type = type_of_violence,
-         number_deaths = best)
-events <- mutate(events,
-                 type = case_when(
-                   type == 1 ~ "state",
-                   type == 2 ~ "Nstate",
-                   type == 3 ~ "onesided"
-                 ))
-
-# Set the coordinate system
-events <- st_as_sf(events, coords = c("longitude", "latitude"), crs = st_crs(shp))
-events <- st_transform(events, st_crs(shp))
-
-# Remove the invalid geometries from shp
-shp <- shp[st_is_valid(shp), ]
-
+events <- st_read("^Data/separate/events_cooridinates/events_coordinates.shp")
 
 events1 <- subset(events, year >= 1989 & year <= 1999)
 events2 <- subset(events, year >= 2000 & year <= 2009)
