aff7ac4ca1a988b6b4883a3ddc1766deabe4f794
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-05-24 11:59:58 +0200

    Update 201.GWS_Events.R

--- 201.GWS_Events.R
+++ 201.GWS_Events.R
@@ -105,5 +105,44 @@ table_Camerica <- xtable(etable(Camerica), tex=TRUE)
 print(table_Camerica, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts_Camerica.tex", include.rownames = FALSE)
 
 
+#################################################################################################
+#################################################################################################
+####  GENERALIZED LINEAR REGRESSION FOR TYPE OF CONFLICTS DATA
+
+state <- subset(ge, type=="state")
+state <- fixest::feglm(data=state, conflicts~sw(gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_anomalies5,gws_anomalies10,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10)|region + year, family=quasipoisson)
+table_state<- xtable(etable(state), tex=TRUE)
+print(table_state, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts_state.tex", include.rownames = FALSE)
+
+Nstate <- subset(ge, type=="Nstate")
+Nstate <- fixest::feglm(data=Nstate, conflicts~sw(gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_anomalies5,gws_anomalies10,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10)|region + year, family=quasipoisson)
+table_Nstate<- xtable(etable(Nstate), tex=TRUE)
+print(table_Nstate, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts_Nstate.tex", include.rownames = FALSE)
+
+onesided <- subset(ge, type=="onesided")
+onesided <- fixest::feglm(data=onesided, conflicts~sw(gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_anomalies5,gws_anomalies10,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10)|region + year, family=quasipoisson)
+table_onesided<- xtable(etable(onesided), tex=TRUE)
+print(table_onesided, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts_onesided.tex", include.rownames = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
