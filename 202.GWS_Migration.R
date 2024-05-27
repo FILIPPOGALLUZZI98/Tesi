ae54a81b89e58bcabffffe240b21219efbd48305
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-05-27 15:05:18 +0200

    Update 202.GWS_Migration.R

--- 202.GWS_Migration.R
+++ 202.GWS_Migration.R
@@ -23,7 +23,7 @@ suppressPackageStartupMessages({
 gm <- read.csv("^Data/gws_migr.csv")
 
 # Setting of the dictionary for the tables
-setFixest_dict(c(migrants="number of migrants/pop in interval", value="gws [g/m^3]",
+setFixest_dict(c(migrants="# migrants/pop", value="gws [g/m^2]",
                  gws_avg1="gws 1-y", gws_avg5="gws 5-y", gws_avg10="gws 10-y",
                  gws_growth1="gws growth rate 1-y", gws_growth5="gws growth rate 5-y", gws_growth10="gws growth rate 10-y",
                  gws_std1="gws st dev 1-y", gws_std5="gws st dev 5-y", gws_std10="gws st dev 10-y",
