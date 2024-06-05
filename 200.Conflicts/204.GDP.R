7889934d7ae22307bd1ec85fdfb710b621cc1a91
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-05 16:33:26 +0200

    Update 204.GDP.R

--- 200.Conflicts/204.GDP.R
+++ 200.Conflicts/204.GDP.R
@@ -12,50 +12,7 @@ suppressPackageStartupMessages({
 
 # Upload of the groundwater-events dataset
 ge <- read.csv("^Data/gws_events.csv")
-
-# Setting of the dictionary for the tables
-setFixest_dict(c(conflicts="# Conflicts", value="gws [g/m^2]",
-                 gws_avg1="gws 1-y", gws_avg5="gws 5-y", gws_avg10="gws 10-y",
-                 gws_growth1="gws growth rate 1-y", gws_growth5="gws growth rate 5-y", gws_growth10="gws growth rate 10-y",
-                 gws_std1="gws st dev 1-y", gws_std5="gws st dev 5-y", gws_std10="gws st dev 10-y",
-                 gws_anomalies="gws anomalies 1y (1980-2010)", gws_anomalies5="gws anomalies 5y (1980-2010)",
-                 gws_anomalies10="gws anomalies 10y (1980-2010)",count="# conflicts"))
-
-gdp_data <- WDI(indicator = "NY.GDP.MKTP.PP.KD", start = 1988, end = 2022, extra = TRUE)
-gdp_data <- subset(gdp_data, year== 2019)
-
-gdp_high <- subset(gdp_data, income == "High income"); name_high <- unique(gdp_high$country)
-gdp_low <- subset(gdp_data, income == "Low income"); name_low <- unique(gdp_low$country)
-gdp_lowmid <- subset(gdp_data, income == "Lower middle income"); name_lowmid <- unique(gdp_lowmid$country)
-gdp_highmid <- subset(gdp_data, income == "Upper middle income"); name_highmid <- unique(gdp_highmid$country)
-
-ge <- subset(ge, type=="state" & year>1989)
-
-ge_high <- subset(ge, country %in% name_high)
-ge_low <- subset(ge, country %in% name_low)
-ge_highmid <- subset(ge, country %in% name_highmid)
-ge_lowmid <- subset(ge, country %in% name_lowmid)
-
-
-high <- fixest::feglm(data=ge_high, count~sw(gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_anomalies5,gws_anomalies10,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10)|region + year, family=quasipoisson)
-etable(high)
-
-low <- fixest::feglm(data=ge_low, count~sw(gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_anomalies5,gws_anomalies10,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10)|region + year, family=quasipoisson)
-etable(low)
-
-midhigh <- fixest::feglm(data=ge_highmid, count~sw(gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_anomalies5,gws_anomalies10,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10)|region + year, family=quasipoisson)
-etable(midhigh)
-
-lowmid <- fixest::feglm(data=ge_lowmid, count~sw(gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_anomalies5,gws_anomalies10,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10)|region + year, family=quasipoisson)
-etable(lowmid)
-
-
-#################################################################################################
-#################################################################################################
-####  GENERALIZED LINEAR REGRESSION (NORMALIZED)
-
-# Upload of the groundwater-events dataset
-ge <- read.csv("^Data/gws_events_normalized.csv")
+# ge <- read.csv("^Data/gws_events_normalized.csv")
 
 # Setting of the dictionary for the tables
 setFixest_dict(c(conflicts="# Conflicts", value="gws [g/m^2]",
