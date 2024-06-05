2f7ceb4b2f2b27edc9dbf608d9be6e83aecb3ec4
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-05 14:32:19 +0200

    Update 102.New_Variables.R

--- 100.Dataset/102.New_Variables.R
+++ 100.Dataset/102.New_Variables.R
@@ -1,3 +1,11 @@
+###### PACKAGES
+
+suppressPackageStartupMessages({
+  library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
+  library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign); library(countrycode);
+  library(fixest); library(broom);library(knitr); library(stargazer); library(xtable)} )
+
+
 #################################################################################################
 #################################################################################################
 ######  DEFINE NEW VARIABLES FOR GW - EVENTS
