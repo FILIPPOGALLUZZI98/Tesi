24cc548c429409bab60a26f7a71d1607cff801ff
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-05 16:32:18 +0200

    Update 201.Global.R

--- 200.Conflicts/201.Global.R
+++ 200.Conflicts/201.Global.R
@@ -12,6 +12,7 @@ suppressPackageStartupMessages({
 
 # Upload of the groundwater-events dataset
 ge <- read.csv("^Data/gws_events.csv")
+# ge <- read.csv("^Data/gws_events_normalized.csv")
 
 # Setting of the dictionary for the tables
 setFixest_dict(c(conflicts="# conflicts", value="gws [Kg/m^2]",
@@ -27,32 +28,12 @@ events_sum <- subset(ge, type=="state" & year>1988)
 
 model <- fixest::feglm(data=events_sum, count~sw(gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_anomalies5, gws_anomalies10, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
 etable(model)
+
 
 # table <- xtable(etable(model), tex=TRUE)
 # print(table, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts.tex", include.rownames = FALSE)
 
 
-#################################################################################################
-#################################################################################################
-####  GENERALIZED LINEAR REGRESSION FOR GLOBAL DATA (NORMALIZED)
-
-# Upload of the groundwater-events dataset
-ge <- read.csv("^Data/gws_events_normalized.csv")
-
-# Setting of the dictionary for the tables
-setFixest_dict(c(conflicts="# conflicts", value="gws [Kg/m^2]",
-                 gws_avg1="gws 1-y", gws_avg5="gws 5-y", gws_avg10="gws 10-y",
-                 gws_growth1="gws growth rate 1-y", gws_growth5="gws growth rate 5-y", gws_growth10="gws growth rate 10-y",
-                 gws_std1="gws st dev 1-y", gws_std5="gws st dev 5-y", gws_std10="gws st dev 10-y",
-                 gws_anomalies="gws anomalies 1y (1980-2010)", gws_anomalies5="gws anomalies 5y (1980-2010)",
-                 gws_anomalies10="gws anomalies 10y (1980-2010)",count="# conflict"))
-
-
-# Create a subset of the dataset (because the variables are counted thrice (one for each type of conflict)
-events_sum <- subset(ge, type=="state" & year>1988)
-
-model <- fixest::feglm(data=events_sum, count~sw(gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_anomalies5, gws_anomalies10, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
-etable(model)
 
 
 
