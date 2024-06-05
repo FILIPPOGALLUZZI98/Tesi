ae87f675d90c3c18b73a3cd1f231cc6cfa4bf90f
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-05 16:33:42 +0200

    Update 301.Global.R

--- 300.Migration/301.Global.R
+++ 300.Migration/301.Global.R
@@ -12,6 +12,7 @@ suppressPackageStartupMessages({
 
 # Upload of the groundwater-migrations dataset
 gm <- read.csv("^Data/gws_migr.csv")
+# gm <- read.csv("^Data/gws_migr_normalized.csv")
 
 # Setting of the dictionary for the tables
 setFixest_dict(c(migrants="# migrants/pop", value="gws [g/m^2]",
@@ -32,28 +33,3 @@ model <- fixest::feglm(data=data_5, log(migrants)~sw(gws_avg1,gws_avg5,gws_avg10
 etable(model)
 
 
-
-#################################################################################################
-#################################################################################################
-####  GENERALIZED LINEAR REGRESSION FOR GLOBAL DATA (NORMALIZED)
-
-# Upload of the groundwater-migrations dataset
-gm <- read.csv("^Data/gws_migr_normalized.csv")
-
-# Setting of the dictionary for the tables
-setFixest_dict(c(migrants="# migrants/pop", value="gws [g/m^2]",
-                 gws_avg1="gws 1-y", gws_avg5="gws 5-y", gws_avg10="gws 10-y",
-                 gws_growth1="gws growth rate 1-y", gws_growth5="gws growth rate 5-y", gws_growth10="gws growth rate 10-y",
-                 gws_std1="gws st dev 1-y", gws_std5="gws st dev 5-y", gws_std10="gws st dev 10-y",
-                 gws_anomalies="gws anomalies 1y (1980-2010)", gws_anomalies5="gws anomalies 5y (1980-2010)",
-                 gws_anomalies10="gws anomalies 10y (1980-2010)"))
-
-# 1-y Migration data
-data_1 <- subset(gm, interval==1)
-model <- fixest::feglm(data=data_1, log(migrants)~sw(gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_anomalies5, gws_anomalies10, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=gaussian)
-etable(model)
-
-# 5-y Migration data
-data_5 <- subset(gm, interval==5)
-model <- fixest::feglm(data=data_5, log(migrants)~sw(gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_anomalies5, gws_anomalies10, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=gaussian)
-etable(model)
