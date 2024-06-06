24a3f133835a039181e359571b1f8e8c30180736
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-06 11:26:17 +0200

    Update 201.Global.R

--- 200.Conflicts/201.Global.R
+++ 200.Conflicts/201.Global.R
@@ -3,12 +3,12 @@
 suppressPackageStartupMessages({
   library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
   library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign); library(countrycode);
-  library(fixest); library(broom);library(knitr); library(stargazer); library(xtable)} )
+  library(fixest); library(broom);library(knitr); library(stargazer); library(xtable); library(data.table)} )
 
 
 #################################################################################################
 #################################################################################################
-####  GENERALIZED LINEAR REGRESSION FOR GLOBAL DATA
+####  GENERALIZED LINEAR REGRESSION
 
 # Upload of the groundwater-events dataset
 ge <- read.csv("^Data/gws_events.csv")
@@ -26,11 +26,10 @@ setFixest_dict(c(conflicts="# conflicts", value="gws [Kg/m^2]",
 events_sum <- subset(ge, type=="state")
 
 model <- fixest::feglm(data=events_sum, count~sw(gws_avg1,gws_avg5,gws_avg10, gws_anomalies, gws_anomalies5, gws_anomalies10, gws_std1, gws_std5,gws_std10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
-etable(model)
+tabella <- etable(model)
+df <- as.data.frame(tabella)
+write.csv(tabella, "^Tabelle/conflicts_global.csv", row.names = FALSE)
 
-
-# table <- xtable(etable(model), tex=TRUE)
-# print(table, caption = "Total Conflicts", caption.placement = "top", file = "^Tables/glm_conflicts.tex", include.rownames = FALSE)
 
 
 
