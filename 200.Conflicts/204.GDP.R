5cd1dcb9a650d5bca2e893e18e14777e5a549581
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-07 11:46:04 +0200

    Update 204.GDP.R

--- 200.Conflicts/204.GDP.R
+++ 200.Conflicts/204.GDP.R
@@ -1,54 +1,77 @@
 # In this code statistical analysis is performed for GWS and Conflicts data divided by GDP
+# Generalized linear regression with fixed effects (region and year)
+# Family: quasipoisson
 
 suppressPackageStartupMessages({
   library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
   library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign); library(countrycode);
-  library(fixest); library(broom);library(knitr); library(stargazer); library(xtable); library(WDI)} )
+  library(fixest);library(xtable); library(data.table)} )
 
 
 #################################################################################################
 #################################################################################################
-####  GENERALIZED LINEAR REGRESSION
 
 # Upload of the groundwater-events dataset
 ge <- read.csv("^Data/gws_events.csv")
-# ge <- read.csv("^Data/gws_events_normalized.csv")
 
 # Setting of the dictionary for the tables
-setFixest_dict(c(conflicts="# Conflicts", value="gws [g/m^2]",
-                 gws_avg1="gws 1-y", gws_avg5="gws 5-y", gws_avg10="gws 10-y",
-                 gws_growth1="gws growth rate 1-y", gws_growth5="gws growth rate 5-y", gws_growth10="gws growth rate 10-y",
-                 gws_std1="gws st dev 1-y", gws_std5="gws st dev 5-y", gws_std10="gws st dev 10-y",
-                 gws_anomalies="gws anomalies 1y (1980-2010)", gws_anomalies5="gws anomalies 5y (1980-2010)",
-                 gws_anomalies10="gws anomalies 10y (1980-2010)",count="# conflicts"))
+setFixest_dict(c(conflicts="# conflicts", value="gws [Kg/m^2]",
+                 gws_avg1="average 1-y", gws_avg5="average 5-y", gws_avg10="average 10-y",
+                 gws_growth1="growth rate (%) 1-y", gws_growth5="growth rate (%) 5-y", gws_growth10="growth rate (%) 10-y",
+                 gws_std1="STD 1-y", gws_std5="STD 5-y", gws_std10="STD 10-y",
+                 gws_anomalies="anomalies 1y", gws_anomalies5="gws anomalies 5y",
+                 gws_anomalies10="gws anomalies 10y",count="# conflicts", 
+                 n_confl="normalized conflicts", n_count="normalized conflicts", n_value="normalized gws",
+                 n_gws_avg1="normalized average 1-y", n_gws_avg5="normalized average 5-y,n_gws_avg10="normalized average 10-y,
+                 CV1="Coefficient of variation 1-y", CV5="Coefficient of variation 5-y",CV10="Coefficient of variation 10-y"))
 
+# Select the GDP list for one year (2019)
 gdp_data <- WDI(indicator = "NY.GDP.MKTP.PP.KD", start = 1988, end = 2022, extra = TRUE)
 gdp_data <- subset(gdp_data, year== 2019)
 
+# Divide the countries into four categories
 gdp_high <- subset(gdp_data, income == "High income"); name_high <- unique(gdp_high$country)
 gdp_low <- subset(gdp_data, income == "Low income"); name_low <- unique(gdp_low$country)
 gdp_lowmid <- subset(gdp_data, income == "Lower middle income"); name_lowmid <- unique(gdp_lowmid$country)
 gdp_highmid <- subset(gdp_data, income == "Upper middle income"); name_highmid <- unique(gdp_highmid$country)
 
-ge <- subset(ge, type=="state" & year>1989)
+# Create a subset of the dataset (because the variables are counted thrice (one for each type of conflict)
+ge <- subset(ge, type=="state")
+
+
+# Statistical model and tables
 
 ge_high <- subset(ge, country %in% name_high)
+high <- fixest::feglm(data=ge_high, count~sw(value, n_value,gws_avg1,gws_avg5,gws_avg10, n_gws_avg1,n_gws_avg5,n_gws_avg10, gws_anomalies, gws_anomalies5, gws_anomalies10, gws_std1, gws_std5,gws_std10, CV1, CV5, CV10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
+n_high <- fixest::feglm(data=ge_high, n_count~sw(value, n_value,gws_avg1,gws_avg5,gws_avg10, n_gws_avg1,n_gws_avg5,n_gws_avg10, gws_anomalies, gws_anomalies5, gws_anomalies10, gws_std1, gws_std5,gws_std10, CV1, CV5, CV10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
+tabella <- etable(high); df <- as.data.frame(tabella)
+write.csv(tabella, "^Tabelle/conflicts_highGDP.csv", row.names = FALSE)
+n_tabella <- etable(n_high); df <- as.data.frame(n_tabella)
+write.csv(n_tabella, "^Tabelle/conflicts_highGDP_n.csv", row.names = FALSE)
+
 ge_low <- subset(ge, country %in% name_low)
+low <- fixest::feglm(data=ge_low, count~sw(value, n_value,gws_avg1,gws_avg5,gws_avg10, n_gws_avg1,n_gws_avg5,n_gws_avg10, gws_anomalies, gws_anomalies5, gws_anomalies10, gws_std1, gws_std5,gws_std10, CV1, CV5, CV10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
+n_low <- fixest::feglm(data=ge_low, n_count~sw(value, n_value,gws_avg1,gws_avg5,gws_avg10, n_gws_avg1,n_gws_avg5,n_gws_avg10, gws_anomalies, gws_anomalies5, gws_anomalies10, gws_std1, gws_std5,gws_std10, CV1, CV5, CV10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
+tabella <- etable(low); df <- as.data.frame(tabella)
+write.csv(tabella, "^Tabelle/conflicts_lowGDP.csv", row.names = FALSE)
+n_tabella <- etable(n_low); df <- as.data.frame(n_tabella)
+write.csv(n_tabella, "^Tabelle/conflicts_lowGDP_n.csv", row.names = FALSE)
+
 ge_highmid <- subset(ge, country %in% name_highmid)
+midhigh <- fixest::feglm(data=ge_highmid, count~sw(value, n_value,gws_avg1,gws_avg5,gws_avg10, n_gws_avg1,n_gws_avg5,n_gws_avg10, gws_anomalies, gws_anomalies5, gws_anomalies10, gws_std1, gws_std5,gws_std10, CV1, CV5, CV10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
+n_midhigh <- fixest::feglm(data=ge_highmid, n_count~sw(value, n_value,gws_avg1,gws_avg5,gws_avg10, n_gws_avg1,n_gws_avg5,n_gws_avg10, gws_anomalies, gws_anomalies5, gws_anomalies10, gws_std1, gws_std5,gws_std10, CV1, CV5, CV10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
+tabella <- etable(midhigh); df <- as.data.frame(tabella)
+write.csv(tabella, "^Tabelle/conflicts_midhighGDP.csv", row.names = FALSE)
+n_tabella <- etable(n_midhigh); df <- as.data.frame(n_tabella)
+write.csv(n_tabella, "^Tabelle/conflicts_midhighGDP_n.csv", row.names = FALSE)
+
 ge_lowmid <- subset(ge, country %in% name_lowmid)
-
-
-high <- fixest::feglm(data=ge_high, count~sw(gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_anomalies5,gws_anomalies10,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10)|region + year, family=quasipoisson)
-etable(high)
-
-low <- fixest::feglm(data=ge_low, count~sw(gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_anomalies5,gws_anomalies10,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10)|region + year, family=quasipoisson)
-etable(low)
-
-midhigh <- fixest::feglm(data=ge_highmid, count~sw(gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_anomalies5,gws_anomalies10,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10)|region + year, family=quasipoisson)
-etable(midhigh)
-
-lowmid <- fixest::feglm(data=ge_lowmid, count~sw(gws_avg1,gws_avg5,gws_avg10,gws_anomalies,gws_anomalies5,gws_anomalies10,gws_std1,gws_std5,gws_std10,gws_growth1,gws_growth5,gws_growth10)|region + year, family=quasipoisson)
-etable(lowmid)
+lowmid <- fixest::feglm(data=ge_lowmid, count~sw(value, n_value,gws_avg1,gws_avg5,gws_avg10, n_gws_avg1,n_gws_avg5,n_gws_avg10, gws_anomalies, gws_anomalies5, gws_anomalies10, gws_std1, gws_std5,gws_std10, CV1, CV5, CV10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
+n_lowmid <- fixest::feglm(data=ge_lowmid, n_count~sw(value, n_value,gws_avg1,gws_avg5,gws_avg10, n_gws_avg1,n_gws_avg5,n_gws_avg10, gws_anomalies, gws_anomalies5, gws_anomalies10, gws_std1, gws_std5,gws_std10, CV1, CV5, CV10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
+tabella <- etable(lowmid); df <- as.data.frame(tabella)
+write.csv(tabella, "^Tabelle/conflicts_lowmidGDP.csv", row.names = FALSE)
+n_tabella <- etable(n_lowmid); df <- as.data.frame(n_tabella)
+write.csv(n_tabella, "^Tabelle/conflicts_lowmidGDP_n.csv", row.names = FALSE)
 
 
 
