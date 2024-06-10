6a6c37db6fe33164015f4e498fad537b72ad0acc
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-10 18:37:00 +0200

    Update 205.Governance.R

--- 200.Conflicts/205.Governance.R
+++ 200.Conflicts/205.Governance.R
@@ -1,4 +1,4 @@
-# In this code statistical analysis is performed for GLOBAL GWS and Conflicts data
+# In this code statistical analysis is performed for GWS and Conflicts data divided by GOVERNANCE
 # Generalized linear regression with fixed effects (region and year)
 # Family: quasipoisson
 
@@ -27,8 +27,10 @@ setFixest_dict(c(conflicts="# conflicts", value="gws [Kg/m^2]",
                  CV1="Coefficient of variation 1-y", CV5="Coefficient of variation 5-y",CV10="Coefficient of variation 10-y"))
 
 
+# Create a subset of the dataset (because the variables are counted thrice (one for each type of conflict)
 ge <- subset(ge, type=="state")
 
+# Select the governance list 
 govern <- govern %>%
   slice(1:(n() - 7)) %>%
   select(-c(1,2,4)) %>%
@@ -40,12 +42,13 @@ govern <- govern %>%
   arrange(desc(gov))
 govern <- govern[-214, ]
 
-gov1 <- govern[1:71, ]; name_gov1 <- unique(gov1$country)
-gov2 <- govern[72:142, ]; name_gov2 <- unique(gov2$country)
-gov3 <- govern[143:213, ]; name_gov3 <- unique(gov3$country)
+# Create 3 classes for high, medium and low governance
+gov1 <- govern[1:71, ]; name_gov1 <- unique(gov1$country)  ## high
+gov2 <- govern[72:142, ]; name_gov2 <- unique(gov2$country)  ## medium
+gov3 <- govern[143:213, ]; name_gov3 <- unique(gov3$country)   ## low
 
 
-
+# Statistical model and tables
 
 ge_gov1 <- subset(ge, country %in% name_gov1)
 gov1 <- fixest::feglm(data=ge_gov1, count~sw(value, n_value,gws_avg1,gws_avg5,gws_avg10, n_gws_avg1,n_gws_avg5,n_gws_avg10, gws_anomalies, gws_anomalies5, gws_anomalies10, gws_std1, gws_std5,gws_std10, CV1, CV5, CV10, gws_growth1, gws_growth5, gws_growth10)|region + year, family=quasipoisson)
