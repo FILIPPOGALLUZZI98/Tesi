ebd4c8166171b224f787c3cb39f79788f7b08a08
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-18 17:48:02 +0200

    Update Prova_PET.R

--- Prova_PET.R
+++ Prova_PET.R
@@ -1,21 +1,20 @@
-pet_t <- raster::brick("^Data_Raw/")
 shp <- st_read("^Data/separate/shp/shp.shp")
+pet_t <- raster::brick("^Data_Raw/Global-AI_ET0_v3_annual/et0_v3_yr.tif")
+proj4string(pet_t) <- raster::crs(shp)
 
-# Set the same CRS of the shapefile
-proj4string(pet) <- raster::crs(shp)
+# Reduce the resolution
+factor <- 0.25 / res(pet_t)[1]
+pet_t <- aggregate(pet_t, fact=factor, fun=mean, expand=TRUE)
 
 # Merging data
 pet <- exactextractr::exact_extract(pet_t, shp, fun="mean")
 
-# Add columns for regions and countries
-pet$region <- shp$region ; pet$country <- shp$country; pet$orig<-shp$GEOLEVEL1
 
-# Reshape the dataset into a long form
-pet <- reshape2::melt(pet, id.vars=c("country", "region", "orig"))
 
-# Rename the years
-pet$variable <- gsub("mean.X", "", gw$variable)  # Rimuovi "mean.X"
-pet$year <- as.integer(gsub("\\D", "", pet$variable)) + 1900 
-pet$variable=NULL
-pet <- pet[, c("year","country", "region", "value","orig")]
 
+# Reshape the dataset
+pet$region <- shp$region ; pet$country <- shp$country
+pet <- reshape2::melt(pet, id.vars=c("country", "region"))
+
+pet_t <- pet_t_aggregated
+plot(pet_t)
