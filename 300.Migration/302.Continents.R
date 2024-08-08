d556c1f8f7d9dc5e4aec44ebf6e86706df54b162
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-08-08 12:14:38 +0200

    Update 302.Continents.R

--- 300.Migration/302.Continents.R
+++ 300.Migration/302.Continents.R
@@ -15,9 +15,9 @@ suppressPackageStartupMessages({
 gm <- read.csv("^Data/gws_migr.csv")
 
 # Setting of the dictionary for the tables
-setFixest_dict(c(migrants="# migrants/pop", value="gws [Kg/m^2]",
+setFixest_dict(c(n_migr="norm #migrants/pop", value="gws [Kg/m^2]",
                  gws_avg1="average 1-y", gws_avg5="average 5-y", gws_avg10="average 10-y",
-                 gws_growth1="growth rate (%) 1-y", gws_growth5="growth rate (%) 5-y", gws_growth10="growth rate (%) 10-y",
+                 gws_logret5="log return 5-y", gws_logret10="log return 10-y",
                  gws_std1="STD 1-y", gws_std5="STD 5-y", gws_std10="STD 10-y",
                  gws_anomalies="anomalies 1y", gws_anomalies5="gws anomalies 5y",
                  gws_anomalies10="gws anomalies 10y", n_value="normalized gws",
