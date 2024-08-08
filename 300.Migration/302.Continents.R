d859c443354412da44a91f455e603d72addf7e88
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-08-08 14:01:58 +0200

    Update 302.Continents.R

--- 300.Migration/302.Continents.R
+++ 300.Migration/302.Continents.R
@@ -17,13 +17,12 @@ gm <- read.csv("^Data/gws_migr.csv")
 # Setting of the dictionary for the tables
 setFixest_dict(c(n_migr="norm #migrants/pop", value="gws [Kg/m^2]",
                  gws_avg1="average 1-y", gws_avg5="average 5-y", gws_avg10="average 10-y",
-                 gws_logret5="log return 5-y", gws_logret10="log return 10-y",
+                 gws_logret="log return 1-y",gws_logret5="log return 5-y", gws_logret10="log return 10-y",
                  gws_std1="STD 1-y", gws_std5="STD 5-y", gws_std10="STD 10-y",
                  gws_anomalies="anomalies 1y", gws_anomalies5="gws anomalies 5y",
                  gws_anomalies10="gws anomalies 10y", n_value="normalized gws",
                  n_gws_avg1="normalized average 1-y", n_gws_avg5="normalized average 5-y",n_gws_avg10="normalized average 10-y",
                  CV1="Coefficient of variation 1-y", CV5="Coefficient of variation 5-y",CV10="Coefficient of variation 10-y"))
-
 
 
 ####  ANALYSIS FOR 1-Y INTERVAL
