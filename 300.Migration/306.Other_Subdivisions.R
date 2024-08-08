ae18f710087bb2357b3cb9e4bbb69c6568326b37
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-08-08 15:08:50 +0200

    Update 306.Other_Subdivisions.R

--- 300.Migration/306.Other_Subdivisions.R
+++ 300.Migration/306.Other_Subdivisions.R
@@ -32,7 +32,7 @@ lista_1 <- c("Mauritania", "Mali", "Burkina Faso", "Niger", "Ciad",
                          "Iraq", "Israele", "Giordania", "Kuwait", "Libano", 
                          "Oman", "Palestina", "Qatar", "Siria", "Turchia", 
                          "Emirati Arabi Uniti", "Yemen")
-data_1 <- events_sum[events_sum$country %in% lista_1, ]
+data_1 <- gm[gm$country %in% lista_1, ]
 lista_2 <- c("Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", 
                       "Cabo Verde", "Camerun", "Repubblica Centrafricana", "Ciad", "Comore", 
                       "Repubblica del Congo", "Repubblica Democratica del Congo", "Costa d'Avorio", 
@@ -43,16 +43,16 @@ lista_2 <- c("Angola", "Benin", "Botswana", "Burkina Faso", "Burundi",
                       "Nigeria", "Ruanda", "Sao Tome e Principe", "Senegal", "Seychelles", 
                       "Sierra Leone", "Somalia", "Sudan del Sud", "Sudan", "Tanzania", 
                       "Togo", "Uganda", "Zambia", "Zimbabwe")
-data_2 <- events_sum[events_sum$country %in% lista_2, ]
+data_2 <- gm[gm$country %in% lista_2, ]
 lista_3 <- c("Brunei", "Cambogia", "Timor Est", "Indonesia", "Laos", 
                             "Malaysia", "Myanmar", "Filippine", "Singapore", "Thailandia", 
                             "Vietnam")
-data_3 <- events_sum[events_sum$country %in% lista_3, ]
+data_3 <- gm[gm$country %in% lista_3, ]
 lista_4 <- c("Belize", "Costa Rica", "El Salvador", "Guatemala", "Honduras", 
                               "Nicaragua", "Panama", "Argentina", "Bolivia", "Brasile", 
                               "Cile", "Colombia", "Ecuador", "Guyana", "Paraguay", 
                               "Peru", "Suriname", "Uruguay", "Venezuela")
-data_4 <- events_sum[events_sum$country %in% lista_4, ]
+data_4 <- gm[gm$country %in% lista_4, ]
 
 
 
@@ -61,41 +61,41 @@ data_4 <- events_sum[events_sum$country %in% lista_4, ]
 # Statistical model and tables 1-y
 
 data <- subset(data_1, interval==1)
-mena <- fixest::feglm(data = data, count~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
-tabella <- etable(mena); write.csv(tabella, "^Tabelle/conflicts_mena.csv", row.names = FALSE)
+mena <- fixest::feglm(data = data, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
+tabella <- etable(mena); write.csv(tabella, "^Tabelle/migrations_mena.csv", row.names = FALSE)
 
 data <- subset(data_2, interval==1)
-sub_sahara <- fixest::feglm(data = data, count~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
-tabella <- etable(sub_sahara); write.csv(tabella, "^Tabelle/conflicts_sub_sahara.csv", row.names = FALSE)
+sub_sahara <- fixest::feglm(data = data, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
+tabella <- etable(sub_sahara); write.csv(tabella, "^Tabelle/migrations_sub_sahara.csv", row.names = FALSE)
 
-data <- subset(data_3, interval==1)
-sud_est_asia <- fixest::feglm(data = data, count~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
-tabella <- etable(sud_est_asia); write.csv(tabella, "^Tabelle/conflicts_sud_est_asia.csv", row.names = FALSE)
+#data <- subset(data_3, interval==1)
+#sud_est_asia <- fixest::feglm(data = data, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
+#tabella <- etable(sud_est_asia); write.csv(tabella, "^Tabelle/migrations_sud_est_asia.csv", row.names = FALSE)
 
 data <- subset(data_4, interval==1)
-cs_america <- fixest::feglm(data = data, count~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
-tabella <- etable(cs_america); write.csv(tabella, "^Tabelle/conflicts_cs_america.csv", row.names = FALSE)
+cs_america <- fixest::feglm(data = data, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
+tabella <- etable(cs_america); write.csv(tabella, "^Tabelle/migrations_cs_america.csv", row.names = FALSE)
 
 
 
 
 # Statistical model and tables 5-y
 
-data <- subset(data_1, interval==1)
-mena <- fixest::feglm(data = data, count~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
-tabella <- etable(mena); write.csv(tabella, "^Tabelle/conflicts_mena.csv", row.names = FALSE)
+#data <- subset(data_1, interval==5)
+#mena <- fixest::feglm(data = data, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
+#tabella <- etable(mena); write.csv(tabella, "^Tabelle/migrations_mena.csv", row.names = FALSE)
 
-data <- subset(data_2, interval==5)
-sub_sahara <- fixest::feglm(data = data, count~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
-tabella <- etable(sub_sahara); write.csv(tabella, "^Tabelle/conflicts_sub_sahara.csv", row.names = FALSE)
+#data <- subset(data_2, interval==5)
+#sub_sahara <- fixest::feglm(data = data, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
+#tabella <- etable(sub_sahara); write.csv(tabella, "^Tabelle/migrations_sub_sahara.csv", row.names = FALSE)
 
 data <- subset(data_3, interval==5)
-sud_est_asia <- fixest::feglm(data = data, count~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
-tabella <- etable(sud_est_asia); write.csv(tabella, "^Tabelle/conflicts_sud_est_asia.csv", row.names = FALSE)
+sud_est_asia <- fixest::feglm(data = data, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
+tabella <- etable(sud_est_asia); write.csv(tabella, "^Tabelle/migrations_sud_est_asia.csv", row.names = FALSE)
 
 data <- subset(data_4, interval==5)
-cs_america <- fixest::feglm(data = data, count~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
-tabella <- etable(cs_america); write.csv(tabella, "^Tabelle/conflicts_cs_america.csv", row.names = FALSE)
+cs_america <- fixest::feglm(data = data, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
+tabella <- etable(cs_america); write.csv(tabella, "^Tabelle/migrations_cs_america.csv", row.names = FALSE)
 
 
 
