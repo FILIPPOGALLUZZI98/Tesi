ee2c2beec3a7b0059aa781c85cc914b97eadec92
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-08-14 16:28:50 +0200

    Update 203.Type.R

--- 200.Conflicts/203.Type.R
+++ 200.Conflicts/203.Type.R
@@ -14,30 +14,19 @@ suppressPackageStartupMessages({
 # Upload of the groundwater-events dataset
 ge <- read.csv("^Data/gws_events.csv")
 
-# Setting of the dictionary for the tables
-setFixest_dict(c(conflicts="# conflicts", value="gws [Kg/m^2]",
-                 gws_avg1="average 1-y", gws_avg5="average 5-y", gws_avg10="average 10-y",
-                 gws_logret="log return 1-y",gws_logret5="log return 5-y", gws_logret10="log return 10-y",
-                 gws_std1="STD 1-y", gws_std5="STD 5-y", gws_std10="STD 10-y",
-                 gws_anomalies="anomalies 1y", gws_anomalies5="gws anomalies 5y",
-                 gws_anomalies10="gws anomalies 10y",count="# conflicts", 
-                 n_confl="normalized conflicts", n_count="normalized conflicts", n_value="normalized gws",
-                 n_gws_avg1="normalized average 1-y", n_gws_avg5="normalized average 5-y",n_gws_avg10="normalized average 10-y",
-                 CV1="Coefficient of variation 1-y", CV5="Coefficient of variation 5-y",CV10="Coefficient of variation 10-y"))
-
 # Statistical model and tables
 
 state <- subset(ge, type=="state")
 model <- fixest::feglm(data=state, conflicts~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
-tabella <- etable(model); write.csv(tabella, "^Tabelle/conflicts_state.csv", row.names = FALSE)
+tabella <- etable(model); tabella <- confl_tabella(tabella); write.csv(tabella, "^Tabelle/conflicts_state.csv", row.names = FALSE)
 
 Nstate <- subset(ge, type=="Nstate")
 model <- fixest::feglm(data=Nstate, conflicts~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
-tabella <- etable(model); write.csv(tabella, "^Tabelle/conflicts_Nstate.csv", row.names = FALSE)
+tabella <- etable(model); tabella <- confl_tabella(tabella); write.csv(tabella, "^Tabelle/conflicts_Nstate.csv", row.names = FALSE)
 
 onesided <- subset(ge, type=="onesided")
 model <- fixest::feglm(data=onesided, conflicts~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=quasipoisson)
-tabella <- etable(model); write.csv(tabella, "^Tabelle/conflicts_onesided.csv", row.names = FALSE)
+tabella <- etable(model); tabella <- confl_tabella(tabella); write.csv(tabella, "^Tabelle/conflicts_onesided.csv", row.names = FALSE)
 
 
 
