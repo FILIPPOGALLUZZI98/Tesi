102498d688c6e84f4b0c5e0f46db09c1d29784f0
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-08-14 16:29:59 +0200

    Create 0.Librerie.R

---
+++ 200.Conflicts/0.Librerie.R
@@ -0,0 +1,4 @@
+suppressPackageStartupMessages({
+  library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
+  library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign); library(countrycode);
+  library(fixest);library(xtable); library(data.table)} )
