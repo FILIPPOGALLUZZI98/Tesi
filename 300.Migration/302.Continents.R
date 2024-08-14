cc99c34a1f7734616538bf092d5ef5569ca94def
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-08-14 17:39:42 +0200

    Update 302.Continents.R

--- 300.Migration/302.Continents.R
+++ 300.Migration/302.Continents.R
@@ -2,130 +2,112 @@
 # Generalized linear regression with fixed effects (region and year)
 # Family: gaussian
 
-suppressPackageStartupMessages({
-  library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
-  library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign); library(countrycode);
-  library(fixest);library(xtable); library(data.table)} )
-
-
 #################################################################################################
 #################################################################################################
 
 # Upload of the groundwater-migrations dataset
 gm <- read.csv("^Data/gws_migr.csv")
-
-# Setting of the dictionary for the tables
-setFixest_dict(c(n_migr="norm #migrants/pop", value="gws [Kg/m^2]",
-                 gws_avg1="average 1-y", gws_avg5="average 5-y", gws_avg10="average 10-y",
-                 gws_logret="log return 1-y",gws_logret5="log return 5-y", gws_logret10="log return 10-y",
-                 gws_std1="STD 1-y", gws_std5="STD 5-y", gws_std10="STD 10-y",
-                 gws_anomalies="anomalies 1y", gws_anomalies5="gws anomalies 5y",
-                 gws_anomalies10="gws anomalies 10y", n_value="normalized gws",
-                 n_gws_avg1="normalized average 1-y", n_gws_avg5="normalized average 5-y",n_gws_avg10="normalized average 10-y",
-                 CV1="Coefficient of variation 1-y", CV5="Coefficient of variation 5-y",CV10="Coefficient of variation 10-y"))
-
-
-####  ANALYSIS FOR 1-Y INTERVAL
 data_1 <- subset(gm, interval==1)
-
-continent <- "Africa"
-get_continent <- function(countries) {
-  countrycode(countries, "country.name", "continent")}
-data_continent <- data_1 %>%
-  filter(get_continent(country) == continent)
-Africa <- fixest::feglm(data=data_continent, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
-tabella <- etable(Africa); write.csv(tabella, "^Tabelle/migration_Africa_1.csv", row.names = FALSE)
-
-continent <- "Asia"
-get_continent <- function(countries) {
-  countrycode(countries, "country.name", "continent")}
-data_continent <- data_1 %>%
-  filter(get_continent(country) == continent)
-Asia <- fixest::feglm(data=data_continent, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
-tabella <- etable(Asia); write.csv(tabella, "^Tabelle/migration_Asia_1.csv", row.names = FALSE)
-
-continent <- "Oceania"
-get_continent <- function(countries) {
-  countrycode(countries, "country.name", "continent")}
-data_continent <- data_1 %>%
-  filter(get_continent(country) == continent)
-Oceania <- fixest::feglm(data=data_continent, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
-tabella <- etable(Oceania); write.csv(tabella, "^Tabelle/migration_Oceania_1.csv", row.names = FALSE)
-
-continent <- "Europe"
-get_continent <- function(countries) {
-  countrycode(countries, "country.name", "continent")}
-data_continent <- data_1 %>%
-  filter(get_continent(country) == continent)
-Europe <- fixest::feglm(data=data_continent, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
-tabella <- etable(Europe); write.csv(tabella, "^Tabelle/migration_Europe_1.csv", row.names = FALSE)
-
-North_America <- c("United States", "Canada", "Mexico")
-N_A <- data_1[data_1$country %in% North_America, ]
-Namerica <- fixest::feglm(data=N_A, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
-tabella <- etable(Namerica); write.csv(tabella, "^Tabelle/migration_Namerica_1.csv", row.names = FALSE)
-
-South_America <- c("Argentina", "Bolivia", "Brasile", "Cile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Perù", "Suriname", "Uruguay", "Venezuela", "Aruba", "Falkland Islands", "French Guiana", "South Georgia and the South Sandwich Islands")
-S_A <- data_1[data_1$country %in% South_America, ]
-Samerica <- fixest::feglm(data=S_A, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
-tabella <- etable(Samerica); write.csv(tabella, "^Tabelle/migration_Samerica_1.csv", row.names = FALSE)
-
-Central_America <- c("Belize", "Costa Rica", "El Salvador", "Guatemala", "Honduras", "Nicaragua", "Panama", "Bahamas", "Barbados", "Cuba", "Dominica", "Giamaica", "Haiti", "Trinidad and Tobago", "Sint Maarten", "Saint Vincent and the Grenadines", "Saint Lucia", "Saint Kitts and Nevis", "Puerto Rico", "Dominican Republic", "Grenada", "Martinique", "Saint Martin", "Virgin Islands", "Turks and Caicos Islands", "Cayman Islands", "British Virgin Islands", "Guadeloupe", "Antigua and Barbuda", "Bonaire", "Curacao", "Saint Barthelemy", "Saba", "Saint Eustatius", "Saint Pierre and Miquelon", "British West Indies")
-C_A <- data_1[data_1$country %in% Central_America, ]
-Camerica <- fixest::feglm(data=C_A, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
-tabella <- etable(Camerica); write.csv(tabella, "^Tabelle/migration_Camerica_1.csv", row.names = FALSE)
-
-
-
-####  GENERALIZED LINEAR REGRESSION FOR CONTINENTS DATA (5-Y)
 data_5 <- subset(gm, interval==5)
 
+####  AFRICA
 continent <- "Africa"
 get_continent <- function(countries) {
   countrycode(countries, "country.name", "continent")}
+data_continent <- data_1 %>%
+  filter(get_continent(country) == continent)
+Africa1 <- fixest::feglm(data=data_continent, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
+tabella1 <- etable(Africa1)
 data_continent <- data_5 %>%
   filter(get_continent(country) == continent)
-Africa <- fixest::feglm(data=data_continent, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
-tabella <- etable(Africa); write.csv(tabella, "^Tabelle/migration_Africa_5.csv", row.names = FALSE)
+Africa5 <- fixest::feglm(data=data_continent, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
+tabella5 <- etable(Africa5) 
+tabella <- migr_tabella(tabella1, tabella5)
+write.csv(tabella, "^Tabelle/migration_Africa.csv", row.names = FALSE)
 
+
+####  ASIA
 continent <- "Asia"
 get_continent <- function(countries) {
   countrycode(countries, "country.name", "continent")}
+data_continent <- data_1 %>%
+  filter(get_continent(country) == continent)
+Asia1 <- fixest::feglm(data=data_continent, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
+tabella1 <- etable(Asia1)
 data_continent <- data_5 %>%
   filter(get_continent(country) == continent)
-Asia <- fixest::feglm(data=data_continent, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
-tabella <- etable(Asia); write.csv(tabella, "^Tabelle/migration_Asia_5.csv", row.names = FALSE)
+Asia5 <- fixest::feglm(data=data_continent, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
+tabella5 <- etable(Asia5)
+tabella <- migr_tabella(tabella1, tabella5)
+write.csv(tabella, "^Tabelle/migration_Asia.csv", row.names = FALSE)
 
+
+#### OCEANIA
 continent <- "Oceania"
 get_continent <- function(countries) {
   countrycode(countries, "country.name", "continent")}
-data_continent <- data_5 %>%
+data_continent <- data_1 %>%
   filter(get_continent(country) == continent)
 Oceania <- fixest::feglm(data=data_continent, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
-tabella <- etable(Oceania); write.csv(tabella, "^Tabelle/migration_Oceania_5.csv", row.names = FALSE)
+tabella1 <- etable(Oceania1)
+data_continent <- data_5 %>%
+  filter(get_continent(country) == continent)
+Oceania5 <- fixest::feglm(data=data_continent, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
+tabella5 <- etable(Oceania5)
+tabella <- migr_tabella(tabella1, tabella5)
+write.csv(tabella, "^Tabelle/migration_Oceania.csv", row.names = FALSE)
 
+
+## EUROPE
 continent <- "Europe"
 get_continent <- function(countries) {
   countrycode(countries, "country.name", "continent")}
+data_continent <- data_1 %>%
+  filter(get_continent(country) == continent)
+Europe1 <- fixest::feglm(data=data_continent, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
+tabella1 <- etable(Europe1)
 data_continent <- data_5 %>%
   filter(get_continent(country) == continent)
-Europe <- fixest::feglm(data=data_continent, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
-tabella <- etable(Europe); write.csv(tabella, "^Tabelle/migration_Europe_5.csv", row.names = FALSE)
+Europe5 <- fixest::feglm(data=data_continent, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
+tabella5 <- etable(Europe5)
+tabella <- migr_tabella(tabella1, tabella5)
+write.csv(tabella, "^Tabelle/migration_Europe.csv", row.names = FALSE)
 
+
+####  NORTH AMERICA
 North_America <- c("United States", "Canada", "Mexico")
+N_A <- data_1[data_1$country %in% North_America, ]
+Namerica1 <- fixest::feglm(data=N_A, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
+tabella1 <- etable(Namerica1)
 N_A <- data_5[data_5$country %in% North_America, ]
-Namerica <- fixest::feglm(data=N_A, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
-tabella <- etable(Namerica); write.csv(tabella, "^Tabelle/migration_Namerica_5.csv", row.names = FALSE)
+Namerica5 <- fixest::feglm(data=N_A, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
+tabella5 <- etable(Namerica5)
+tabella <- migr_tabella(tabella1, tabella5)
+write.csv(tabella, "^Tabelle/migration_Namerica.csv", row.names = FALSE)
 
+
+#### SOUTH AMERICA
 South_America <- c("Argentina", "Bolivia", "Brasile", "Cile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Perù", "Suriname", "Uruguay", "Venezuela", "Aruba", "Falkland Islands", "French Guiana", "South Georgia and the South Sandwich Islands")
+S_A <- data_1[data_1$country %in% South_America, ]
+Samerica1 <- fixest::feglm(data=S_A, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
+tabella1 <- etable(Samerica1)
 S_A <- data_5[data_5$country %in% South_America, ]
-Samerica <- fixest::feglm(data=S_A, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
-tabella <- etable(Samerica); write.csv(tabella, "^Tabelle/migration_Samerica_5.csv", row.names = FALSE)
+Samerica5 <- fixest::feglm(data=S_A, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
+tabella5 <- etable(Samerica5)
+tabella <- migr_tabella(tabella1, tabella5)
+write.csv(tabella, "^Tabelle/migration_Samerica.csv", row.names = FALSE)
 
+
+#### CENTRAL AMERICA
 Central_America <- c("Belize", "Costa Rica", "El Salvador", "Guatemala", "Honduras", "Nicaragua", "Panama", "Bahamas", "Barbados", "Cuba", "Dominica", "Giamaica", "Haiti", "Trinidad and Tobago", "Sint Maarten", "Saint Vincent and the Grenadines", "Saint Lucia", "Saint Kitts and Nevis", "Puerto Rico", "Dominican Republic", "Grenada", "Martinique", "Saint Martin", "Virgin Islands", "Turks and Caicos Islands", "Cayman Islands", "British Virgin Islands", "Guadeloupe", "Antigua and Barbuda", "Bonaire", "Curacao", "Saint Barthelemy", "Saba", "Saint Eustatius", "Saint Pierre and Miquelon", "British West Indies")
+C_A <- data_1[data_1$country %in% Central_America, ]
+Camerica1 <- fixest::feglm(data=C_A, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
+tabella1 <- etable(Camerica1)
 C_A <- data_5[data_5$country %in% Central_America, ]
-Camerica <- fixest::feglm(data=C_A, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
-tabella <- etable(Camerica); write.csv(tabella, "^Tabelle/migration_Camerica_5.csv", row.names = FALSE)
+Camerica5 <- fixest::feglm(data=C_A, n_migr~sw(n_value,n_gws_avg5,n_gws_avg10,gws_anomalies, gws_anomalies5, gws_anomalies10,CV1, CV5, CV10,gws_logret, gws_logret5, gws_logret10)|region + year, family=gaussian)
+tabella5 <- etable(Camerica5)
+tabella <- migr_tabella(tabella1, tabella5)
+write.csv(tabella, "^Tabelle/migration_Camerica.csv", row.names = FALSE)
 
 
 
