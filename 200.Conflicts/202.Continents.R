bf1a12376b5c650667abcb2659ae565dd5988c40
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-08-14 16:30:52 +0200

    Update 202.Continents.R

--- 200.Conflicts/202.Continents.R
+++ 200.Conflicts/202.Continents.R
@@ -2,18 +2,11 @@
 # Generalized linear regression with fixed effects (region and year)
 # Family: quasipoisson
 
-suppressPackageStartupMessages({
-  library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
-  library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign); library(countrycode);
-  library(fixest);library(xtable); library(data.table)} )
-
-
 #################################################################################################
 #################################################################################################
 
 # Upload of the groundwater-events dataset
 ge <- read.csv("^Data/gws_events.csv")
-
 # Create a subset of the dataset (because the variables are counted thrice (one for each type of conflict)
 events_sum <- subset(ge, type=="state")
 
