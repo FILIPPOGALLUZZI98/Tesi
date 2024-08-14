53e2a494aa42d649865e5ad2f1ac0521caa4d1fc
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-08-14 17:56:35 +0200

    Update 300.Librerie.R

--- 300.Migration/300.Librerie.R
+++ 300.Migration/300.Librerie.R
@@ -1,5 +1,5 @@
 suppressPackageStartupMessages({
   library(sf);library(sp);library(plyr);library(raster);library(ncdf4);library(exactextractr);library(dplyr);library(stringr)
   library(reshape2);library(ggplot2);library(ggrepel);library(lubridate);library(zoo);library(foreign); library(countrycode);
-  library(fixest);library(xtable); library(data.table)} )
+  library(fixest);library(xtable); library(data.table); library(WDI)} )
 
