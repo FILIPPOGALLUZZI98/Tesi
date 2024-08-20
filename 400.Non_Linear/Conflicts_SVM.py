33a8963b4afa83a34bb58e63157fb5a9d99be155
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-08-20 11:27:09 +0200

    Update Conflicts_SVM.py

--- 400.Non_Linear/Conflicts_SVM.py
+++ 400.Non_Linear/Conflicts_SVM.py
@@ -1,4 +1,28 @@
-e = pd.read_csv(datadir+'gws_events.csv')
+# Montaggi GDrive
+import os, sys
+from google.colab import drive
+drivedir='/content/drive'
+drive.mount(drivedir)
+os.chdir(drivedir)
+datadir=drivedir+'/MyDrive/'
+
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error,  r2_score
+from sklearn.metrics import accuracy_score
+from xgboost import XGBClassifier
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_selection import RFE
+from sklearn.inspection import PartialDependenceDisplay
+
+
+ge = pd.read_csv(datadir+'gws_events.csv')
 features = ['n_value', 'n_gws_avg5', 'n_gws_avg10', 'gws_anomalies', 'gws_anomalies5', 'gws_anomalies10',
             'CV1', 'CV5', 'CV10','gws_logret', 'gws_logret5', 'gws_logret10']
 
